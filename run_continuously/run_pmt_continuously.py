1bbb846ad569b9417635693084752b105762d979
Joseph Broz <jbroz@berkeley.edu>
2019-03-22 17:55:16 -0700

    t0

--- run_continuously/run_pmt_continuously.py
+++ run_continuously/run_pmt_continuously.py
@@ -34,7 +34,7 @@ class pmt_collect_continuously(EnvExperiment):
         self.dds_866.sw.on()
         self.dds_397.sw.on()
         while True:
-            t_count = self.pmt.gate_both(self.duration*ms)
+            t_count = self.pmt.gate_rising(self.duration*ms)
             pmt_count = self.pmt.count(t_count)
             self.append_to_dataset("pmt_counts", pmt_count)
             self.core.break_realtime()
