83a30dce7a41dc68b852675e406df8153f4968d0
Joseph Broz <jbroz@berkeley.edu>
2019-04-01 18:32:14 -0700

    t

--- run_continuously/idle_kernel.py
+++ run_continuously/idle_kernel.py
@@ -4,18 +4,24 @@ from artiq.experiment import *
 class IdleKernel(EnvExperiment):
     def build(self):
         self.setattr_device("core")
-        self.setattr_device("led0")
+        self.cpld = self.get_device("urukul0_cpld")
+        self.dds_866 = self.get_device("866")
+        self.dds_397 = self.get_device("397")
 
     @kernel
     def run(self):
-        start_time = now_mu() + self.core.seconds_to_mu(500*ms)
-        while self.core.get_rtio_counter_mu() < start_time:
-            pass
+        #start_time = now_mu() + self.core.seconds_to_mu(500*ms)
+        #while self.core.get_rtio_counter_mu() < start_time:
+        #    continue
         self.core.reset()
-        while True:
-            self.led0.pulse(250*ms)
-            delay(126*ms)
-            self.led0.pulse(125*ms)
-            delay(125*ms)
-            self.led0.pulse(125*ms)
-            delay(250*ms)
+        self.cpld.init()
+        self.dds_866.init()
+        self.dds_397.init()
+        self.core.break_realtime()
+        self.dds_866.sw.on()
+        self.dds_397.sw.on()
+        self.dds_866.set(100*MHz, amplitude=1.0)
+        self.dds_397.set(150*MHz, amplitude=1.0)
+        self.dds_866.set_att(22*dB)
+        self.dds_397.set_att(22*dB)
+
