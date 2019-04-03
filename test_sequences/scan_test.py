e150ba98bbd0203f12065679baf9978a0e402118
Joseph Broz <jbroz@berkeley.edu>
2019-04-03 13:05:01 -0700

    t

--- test_sequences/scan_test.py
+++ test_sequences/scan_test.py
@@ -1,4 +1,4 @@
-#from artiq.language import *
+from artiq.language import *
 from artiq.experiment import *
 
 
@@ -12,40 +12,44 @@ class scanTest(EnvExperiment):
         self.dds_866 = self.get_device("866")
 
     def run(self):
-        self.set_dataset("pmt_counts", [])
-        self.set_dataset("time", [])
-        self.cpld.init()
-        self.dds_866.init()
-        self.dds_397.init()
+        self.core.reset()
+        #self.set_dataset("pmt_counts", [])
+        #self.set_dataset("time", [])
         while True:
             self.scheduler.pause()
             self.kernel_run()
+        except TerminationRequested:
+            break
 
     @kernel
     def kernel_run(self):
         self.core.break_realtime()
+        self.cpld.init()
+        self.dds_397.init()
+        self.dds_866.init()
+        self.dds_397.set(10*MHz, profile=0)
+        self.dds_866.set(10*MHz, profile=0)
+        self.dds_397.set_att(22*dB)
+        self.dds_866.set_att(15*dB)
+        self.dds_397.sw.on()
+        self.dds_866.sw.on()
 
-        self.dds_397.set(800*MHz)
-        self.dds_866.set(500*MHz)
-        #self.dds_397.sw.on()
-        #self.dds_866.sw.on()
+        delay(1*s)
 
-        #self.core.delay(1*s)
+        self.dds_397.sw.off()
+        self.dds_866.sw.off()
 
-        #self.dds_397.sw.off()
-        #self.dds_866.sw.off()
+        delay(1*s)
 
-        #self.core.delay(1*s)
+        self.dds_397.set(500*MHz, profile=0)
+        self.dds_866.set(500*MHz, profile=0)
+        self.dds_397.sw.on()
+        self.dds_866.sw.on()
 
-        #self.dds_397.set(20*MHz)
-        #self.dds_866.set(20*MHz)
-        #self.dds_397.sw.on()
-        #self.dds_866.sw.on()
+        delay(1*s)
 
-        #self.core.delay(1*s)
-
-        #self.dds_397.sw.off()
-        #self.dds_866.sw.off()
+        self.dds_397.sw.off()
+        self.dds_866.sw.off()
 
 
 
