4659987751906f6f6bbdf4c3519f542c5ffcecc4
Joseph Broz <jbroz@berkeley.edu>
2019-04-03 16:08:10 -0700

    t

--- test_sequences/scan_test.py
+++ test_sequences/scan_test.py
@@ -32,25 +32,22 @@ class scanTest(EnvExperiment):
         self.dds_866.set(10*MHz)
         self.dds_397.set_att(22*dB)
         self.dds_866.set_att(15*dB)
-        self.dds_397.sw.on()
-        self.dds_866.sw.on()
+        self.dds_397.sw.pulse(1*s)
+        self.dds_866.sw.pulse(1*s)
 
         delay(1*s)
 
-        self.dds_397.sw.off()
-        self.dds_866.sw.off()
+        #self.dds_397.sw.off()
+        #self.dds_866.sw.off()
 
-        delay(1*s)
+        #delay(1*s)
 
         self.dds_397.set(100*MHz)
         self.dds_866.set(100*MHz)
-        self.dds_397.sw.on()
-        self.dds_866.sw.on()
+        self.dds_397.sw.pulse(1*s)
+        self.dds_866.sw.pulse(1*s)
 
         delay(1*s)
-
-        self.dds_397.sw.off()
-        self.dds_866.sw.off()
 
 
 
