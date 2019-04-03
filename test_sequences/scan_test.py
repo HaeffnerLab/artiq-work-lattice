1906660fcbbac5fef62d84e7ae7af522aa2862c9
Joseph Broz <jbroz@berkeley.edu>
2019-04-03 13:46:04 -0700

    t

--- test_sequences/scan_test.py
+++ test_sequences/scan_test.py
@@ -14,8 +14,6 @@ class scanTest(EnvExperiment):
 
     def run(self):
         self.core.reset()
-        #self.set_dataset("pmt_counts", [])
-        #self.set_dataset("time", [])
         for _ in self.scan:
             try:
                 self.scheduler.pause()
@@ -27,31 +25,35 @@ class scanTest(EnvExperiment):
     def kernel_run(self):
         self.core.break_realtime()
         self.cpld.init()
-        self.dds_397.init()
-        self.dds_866.init()
-        self.dds_397.set(10*MHz, profile=0)
-        self.dds_866.set(10*MHz, profile=0)
-        self.dds_397.set_att(22*dB)
-        self.dds_866.set_att(15*dB)
-        self.dds_397.sw.on()
-        self.dds_866.sw.on()
+        with parallel:
+            self.dds_397.init()
+            self.dds_866.init()
+            self.dds_397.set(10*MHz, profile=0)
+            self.dds_866.set(10*MHz, profile=0)
+            self.dds_397.set_att(22*dB)
+            self.dds_866.set_att(15*dB)
+            self.dds_397.sw.on()
+            self.dds_866.sw.on()
 
         delay(1*s)
 
-        self.dds_397.sw.off()
-        self.dds_866.sw.off()
+        with parallel:
+            self.dds_397.sw.off()
+            self.dds_866.sw.off()
 
         delay(1*s)
 
-        self.dds_397.set(100*MHz, profile=0)
-        self.dds_866.set(100*MHz, profile=0)
-        self.dds_397.sw.on()
-        self.dds_866.sw.on()
+        with parallel:  
+            self.dds_397.set(100*MHz, profile=0)
+            self.dds_866.set(100*MHz, profile=0)
+            self.dds_397.sw.on()
+            self.dds_866.sw.on()
 
         delay(1*s)
 
-        self.dds_397.sw.off()
-        self.dds_866.sw.off()
+        with parallel:
+            self.dds_397.sw.off()
+            self.dds_866.sw.off()
 
 
 
