1f931f37ad71ec8a21174c12bea75f9db59f8c74
Joseph Broz <jbroz@berkeley.edu>
2019-04-11 15:12:57 -0700

    t

---
+++ test_sequences/test_plotter.py
@@ -0,0 +1,23 @@
+import time
+from artiq.experiment import *
+from artiq.protocols.pc_rpc import Client
+
+
+class FloppingF(EnvExperiment):
+
+    def run(self):
+        c = Client("::1", 3286, "rcg")
+		i = 1
+		self.set_dataset("x", [])
+		self.set_dataset("y", [])
+        while True:
+			self.append_to_dataset("x", i)
+            self.append_to_dataset("y", i)
+            x = self.get_dataset("x")
+            y = self.get_dataset("y")
+            self.send_to_rcg(x, y)
+            time.sleep(.5)
+
+    @rpc(flags={"async"})
+    def send_to_rcg(self, x, y):
+        c.plot(x, y)
