34ab87c1ad7ab09c8773d057e3ea2011e8632056
Joseph Broz <jbroz@berkeley.edu>
2019-04-12 16:10:14 -0700

    t

--- test_sequences/test_cw.py
+++ test_sequences/test_cw.py
@@ -7,9 +7,9 @@ class change_cw(EnvExperiment):
         self.setattr_device("core")
         self.setattr_device("scheduler")
         self.dds = self.get_device("866")
-        self.frequency = self.get_argument("amplitude", NumberValue(80, unit="MHz", scale=0.1))
-        self.get_argument("frequency", NumberValue(-40, unit="dB", scale=0.1))
-        self.get_argument("state", BooleanValue())
+        self.amplitude = self.get_argument("amplitude", NumberValue(80, unit="MHz", scale=0.1))
+        self.frequency = self.get_argument("frequency", NumberValue(-40, unit="dB", scale=0.1))
+        self.state = self.get_argument("state", BooleanValue())
 
     @kernel
     def run(self):
