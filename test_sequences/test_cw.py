acc63a8ac3a3e4a0ff346af3ccd659f413e04e69
Joseph Broz <jbroz@berkeley.edu>
2019-04-12 17:22:55 -0700

    t

--- test_sequences/test_cw.py
+++ test_sequences/test_cw.py
@@ -1,15 +1,15 @@
 from artiq import *
 from artiq.language import *
 
-class change_cw(EnvExperiment):
+class change_cw1(EnvExperiment):
 
     def build(self):
         self.setattr_device("core")
         self.setattr_device("scheduler")
         self.dds = self.get_device("866")
         self.cpld = self.get_device("urukul0_cpld")
-        self.frequency = self.get_argument("frequency", NumberValue(80, unit="MHz", scale=1e7))
-        self.amplitude = self.get_argument("amplitude", NumberValue(30, unit="dB", scale=10))
+        self.frequency = self.get_argument("frequency", NumberValue(80, unit="MHz"))
+        self.amplitude = self.get_argument("amplitude", NumberValue(30, unit="dB"))
         self.state = self.get_argument("state", BooleanValue())
 
     @kernel
