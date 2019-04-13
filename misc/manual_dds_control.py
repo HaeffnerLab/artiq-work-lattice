1d42be1ce619f8728bb3e41259ad4af09df52e73
Joseph Broz <jbroz@berkeley.edu>
2019-04-12 18:51:29 -0700

    t

---
+++ misc/manual_dds_control.py
@@ -0,0 +1,38 @@
+from artiq import *
+from artiq.language import *
+
+class change_cw(EnvExperiment):
+
+    def build(self):
+        self.setattr_device("core")
+        self.setattr_device("scheduler")
+        self.frequency = self.get_argument("frequency", NumberValue(80, unit="MHz"))
+        self.amplitude = self.get_argument("amplitude", NumberValue(30, unit="dB"))
+        self.state = self.get_argument("state", BooleanValue())
+        urukul_number = self.get_argument("urukul_number", NumberValue(0, ndecimals=0, scale=1))
+        dds_name = self.get_argument("dds_name", StringValue("397"))
+        self.dds = self.get_device(dds_name)
+        self.cpld = self.get_device("urukul{}_cpld".format(urukul_number))
+
+    @kernel
+    def run(self):
+        self.core.reset()
+        self.cpld.init()
+        self.dds.init()
+        if self.scheduler.check_pause():
+            return
+        if self.state:
+            self.dds.sw.on()
+        else:
+            self.dds.sw.off()
+        self.dds.set(self.frequency)
+        self.dds.set_att(self.amplitude)
+
+    def analyze(self):
+        pass
+        #print(self.frequency)
+        #print(self.frequency*MHz)
+        #print(self.amplitude)
+        #print(self.amplitude*dB)
+
+
