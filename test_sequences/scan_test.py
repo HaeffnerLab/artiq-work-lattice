ead3e9a57b10e9983eaee1d650ed7754d144bfc4
Joseph Broz <jbroz@berkeley.edu>
2019-04-13 16:09:30 -0700

    t

--- test_sequences/scan_test.py
+++ test_sequences/scan_test.py
@@ -84,7 +84,7 @@ class scanTest(EnvExperiment):
                 self.record_result("y1", (i, j), y1val)
                 self.record_result("y2", (i, j), y2val)
             self.record_result("x", i, xval)
-            dp = sum(self.y1) / self.N
+            dp = sum(self.y1[i]) / self.N
             self.record_result("yfull", i, dp)
             self.send_to_rcg("x", self.yfull)
             time.sleep(0.5)
