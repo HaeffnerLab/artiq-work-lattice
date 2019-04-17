46035fa5918ad4c925d794ec80f281e0ab517e82
Joseph Broz <jbroz@berkeley.edu>
2019-04-16 17:42:22 -0700

    t

--- misc/manual_dds_control.py
+++ misc/manual_dds_control.py
@@ -8,7 +8,7 @@ class change_cw(EnvExperiment):
         self.setattr_device("scheduler")
         self.specs = self.get_argument("specs", PYONValue())
         for key in self.specs.keys():
-            print(self.specs[key]["state"])
+            print(type(self.specs[key]["state"]))
         # self.frequency = self.get_argument("frequency", NumberValue(80, unit="MHz"))
         # self.amplitude = self.get_argument("amplitude", NumberValue(30, unit="dB"))
         # self.state = self.get_argument("state", BooleanValue())
