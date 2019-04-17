82222382dc2f5788d3d61295a1da4af9e186ff67
Joseph Broz <jbroz@berkeley.edu>
2019-04-17 09:35:30 -0700

    t

--- misc/manual_dds_control.py
+++ misc/manual_dds_control.py
@@ -29,7 +29,7 @@ class change_cw(EnvExperiment):
         if self.scheduler.check_pause():
             return
         # self.core.reset()
-        self.dds_init()
+        # self.dds_init()
         self.core.break_realtime()
         for dds in list(self.ddss.keys())[0:1]:
             self.set_dds(self.ddss[dds], 
