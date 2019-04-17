c9d5db398dda08e72418c38d5231503b4a35be36
Joseph Broz <jbroz@berkeley.edu>
2019-04-17 16:13:00 -0700

    t

--- run_continuously/run_pmt_pulsed.py
+++ run_continuously/run_pmt_pulsed.py
@@ -28,8 +28,8 @@ class pmt_collect_pulsed(EnvExperiment):
                 self.run_pmt()
                 #self.core.comm.close()
                 self.scheduler.pause()
-                self.core.comm.open()
-                self.core.comm.reset_session()
+                #self.core.comm.open()
+                #self.core.comm.reset_session()
             except (BrokenPipeError, ConnectionResetError) as e:
                 print("exception\n\n", e)
 
