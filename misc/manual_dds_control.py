e9a7f4f34d35136258863a4a34572265c052eef8
Joseph Broz <jbroz@berkeley.edu>
2019-04-16 17:25:29 -0700

    t

--- misc/manual_dds_control.py
+++ misc/manual_dds_control.py
@@ -1,6 +1,5 @@
 from artiq import *
 from artiq.language import *
-# from artiq.protocols import pyon
 
 class change_cw(EnvExperiment):
 
@@ -8,18 +7,13 @@ class change_cw(EnvExperiment):
         self.setattr_device("core")
         self.setattr_device("scheduler")
         self.specs = self.get_argument("specs", PYONValue())
-        for key, val in self.specs.items():
-            print(key, "\n")
-            for i,j in val.items():
-                print(i, " ", j)
-
-        self.frequency = self.get_argument("frequency", NumberValue(80, unit="MHz"))
-        self.amplitude = self.get_argument("amplitude", NumberValue(30, unit="dB"))
-        self.state = self.get_argument("state", BooleanValue())
-        urukul_number = self.get_argument("urukul_number", StringValue("0"))
-        dds_name = self.get_argument("dds_name", StringValue("397"))
-        self.dds = self.get_device(dds_name)
-        self.cpld = self.get_device("urukul{}_cpld".format(urukul_number))
+        # self.frequency = self.get_argument("frequency", NumberValue(80, unit="MHz"))
+        # self.amplitude = self.get_argument("amplitude", NumberValue(30, unit="dB"))
+        # self.state = self.get_argument("state", BooleanValue())
+        # urukul_number = self.get_argument("urukul_number", StringValue("0"))
+        # dds_name = self.get_argument("dds_name", StringValue("397"))
+        self.ddss = {dds_name: self.get_device(dds_name) for dds_name in self.specs.keys()}
+        self.cplds = [self.get_device("urukul{}_cpld".format(i)) for i in range(3)]
 
 
     def prepare(self):
@@ -30,14 +24,25 @@ class change_cw(EnvExperiment):
         if self.scheduler.check_pause():
             return
         self.core.reset()
-        self.cpld.init()
-        self.dds.init()
-        if self.state:
-            self.dds.sw.on()
-        else:
-            self.dds.sw.off()
-        self.dds.set(self.frequency)
-        self.dds.set_att(self.amplitude)
+        for cpld in self.cplds:
+            cpld.init()
+        for dds in self.ddss.values():
+            dds.init()
+        for dds in self.ddss.keys():
+            if self.specs[dds]["state"]:
+                self.ddss[dds].sw.on()
+            else:
+                self.ddss[dds].sw.off()
+            self.ddss[dds].set(self.specs[dds]["frequency"])
+            self.ddss[dds].set_att(self.specs[dds]["att"])
+        
+        
+        # if self.state:
+        #     self.dds.sw.on()
+        # else:
+        #     self.dds.sw.off()
+        # self.dds.set(self.frequency)
+        # self.dds.set_att(self.amplitude)
 
     def analyze(self):
         pass
