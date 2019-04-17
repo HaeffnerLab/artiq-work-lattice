c079135d28f0ef5c251beae1089e161213342fea
Joseph Broz <jbroz@berkeley.edu>
2019-04-17 13:56:45 -0700

    t

--- test_sequences/scan_test.py
+++ test_sequences/scan_test.py
@@ -151,8 +151,8 @@ class scanTest(EnvExperiment):
             self.save_result("yfull2", self.yfull2[-rem:])
             time.sleep(0.5)
 
-        self.reset_cw_settings(self.dds_list, self.freq_list, 
-                               self.amp_list, self.state_list, self.att_list)
+        # self.reset_cw_settings(self.dds_list, self.freq_list, 
+        #                        self.amp_list, self.state_list, self.att_list)
             
     @rpc(flags={"async"})
     def send_to_rcg(self, x, y, name):
