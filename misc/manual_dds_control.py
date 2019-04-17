a3542c73c3122a3bcd361be74f83a5837da4944a
Joseph Broz <jbroz@berkeley.edu>
2019-04-17 10:52:50 -0700

    t

--- misc/manual_dds_control.py
+++ misc/manual_dds_control.py
@@ -7,32 +7,47 @@ class change_cw(EnvExperiment):
         self.setattr_device("core")
         self.setattr_device("scheduler")
         self.specs = self.get_argument("specs", PYONValue())
-        print(self.specs)
         # self.frequency = self.get_argument("frequency", NumberValue(80, unit="MHz"))
         # self.amplitude = self.get_argument("amplitude", NumberValue(30, unit="dB"))
         # self.state = self.get_argument("state", BooleanValue())
         # urukul_number = self.get_argument("urukul_number", StringValue("0"))
         # dds_name = self.get_argument("dds_name", StringValue("397"))
         # self.ddss = {dds_name: self.get_device(dds_name) for dds_name in self.specs.keys()}
-        # self.ddss = dict()
-        # try:
-        #     for name in self.specs.keys():
-        #         self.setattr_device(name)
-        #         self.ddss[name] = self.get_device(name)
-        #     self.cplds = [self.get_device("urukul{}_cpld".format(i)) for i in range(2)]
-        # except AttributeError:
-        #     self.cplds = []
-        # print(self.ddss)
-        self.cpld = self.get_device("urukul0_cpld")
-        self.dds = self.get_device("397")
+        self.dds_dict = dict()
+        try:
+            for name in self.specs.keys():
+                self.setattr_device(name)
+                self.dds_dict[name] = self.get_device(name)
+        except AttributeError:
+            pass
 
     def prepare(self):
         self.archive = False
+        self.dds_list = []
+        self.freq_list = []
+        self.att_list = []
+        self.state_list = []
+        for dds, settings in self.specs.items():
+            self.dds_list.append(self.dds_dict[dds])
+            self.freq_list.append(settings["frequency"])
+            self.att_list.append(settings["att"])
+            self.state_list.append(settings["state"])
 
+    @kernel
     def run(self):
+        self.core.reset()
         if self.scheduler.check_pause():
             return
-        self.core.reset()
+        with parallel:
+            for i in range(len(self.dds_list)):
+                self.dds_list[i].init()
+                self.dds_list[i].set(self.freq_list[i])
+                self.dds_list[i].set_att(self.att_list[i]*dB)
+                if self.state_list[i]:
+                    self.dds_list[i].sw.on()
+                else:
+                    self.dds_list[i].sw.off()
+
         # self.core.break_realtime()
         # for dds in list(self.ddss.values())[0:1]:
         #     # self.core.break_realtime()
@@ -42,12 +57,6 @@ class change_cw(EnvExperiment):
         #                 self.specs[dds]["state"], 
         #                 self.specs[dds]["frequency"], 
         #                 float(self.specs[dds]["att"]))
-
-        self.cpld.init()
-        self.core.break_realtime()
-        self.dds.init()
-        self.set_dds(self.dds, True, 100*MHz, 10)
-
             # if self.specs[dds][self.state]:
             #     self.ddss[dds].sw.on()
             # else:
@@ -80,26 +89,26 @@ class change_cw(EnvExperiment):
         # self.dds.set(self.frequency)
         # self.dds.set_att(self.amplitude)
 
-    def analyze(self):
-        pass
-        #print(self.frequency)
-        #print(self.frequency*MHz)
-        #print(self.amplitude)
-        #print(self.amplitude*dB)
+    # def analyze(self):
+    #     pass
+    #     #print(self.frequency)
+    #     #print(self.frequency*MHz)
+    #     #print(self.amplitude)
+    #     #print(self.amplitude*dB)
 
-    @kernel
-    def set_dds(self, dds, state, frequency, att):
-        dds.set(frequency)
-        dds.set_att(att*dB)
-        if state:
-            dds.sw.on()
-        else:
-            dds.sw.off()
+    # @kernel
+    # def set_dds(self, dds, state, frequency, att):
+    #     dds.set(frequency)
+    #     dds.set_att(att*dB)
+    #     if state:
+    #         dds.sw.on()
+    #     else:
+    #         dds.sw.off()
 
-    @kernel
-    def init_dds(self, dds):
-        dds.init()
+    # @kernel
+    # def init_dds(self, dds):
+    #     dds.init()
 
-    @kernel
-    def init_cpld(self, cpld):
-        cpld.init()
+    # @kernel
+    # def init_cpld(self, cpld):
+    #     cpld.init()
--- misc/manual_dds_control_test.py
+++ misc/manual_dds_control_test.py
@@ -1,6 +1,7 @@
 from artiq import *
 from artiq.language import *
 
+
 class change_cw11(EnvExperiment):
 
     def build(self):
@@ -9,27 +10,41 @@ class change_cw11(EnvExperiment):
         self.dds1 = self.get_device("866")
         self.cpld = self.get_device("urukul0_cpld")
 
-    @kernel
     def run(self):
         self.core.reset()
         #self.cpld.init()
-        self.dds.init()
-        self.dds1.init()
-        self.dds.set(80*MHz)
-        self.dds.set_att(25*dB)
-        self.dds.sw.on()
-        self.dds1.set(100*MHz)
-        self.dds1.set_att(25*dB)
-        self.dds1.sw.on()
-        #self.set_dds(self.dds, False, 80*MHz, 20)
+        #self.dds_init([self.dds, self.dds1])
+        #self.dds.set(80*MHz)
+        #self.dds.set_att(25*dB)
+        #self.dds.sw.on()
+        #self.dds1.set(100*MHz)
+        #self.dds1.set_att(25*dB)
+        #self.dds1.sw.on()
+        self.set_dds([self.dds, self.dds1], [False, False], [100*MHz, 80*MHz], [10, 20])
+        #self.set_dds(self.dds1, True, 80*MHz, 20)
 
     @kernel
-    def set_dds(self, dds, state, frequency, att):
+    def set_dds(self, dds_list, state_list, frequency_list, att_list):
         self.core.break_realtime()
-        dds.init()
-        if state:
-            dds.sw.on()
-        else:
-            dds.sw.off()
-        dds.set(frequency)
-        dds.set_att(att*dB)
+        with parallel:
+            for i in range(len(dds_list)):
+                dds_list[i].init()
+                dds_list[i].set(frequency_list[i])
+                dds_list[i].set_att(att_list[i]*dB)
+                if state_list[i]:
+                    dds_list[i].sw.on()
+                else:
+                    dds_list[i].sw.off()
+        #dds.init()
+        #dds.set(frequency)
+        #dds.set_att(att*dB)
+        #if state:
+        #    dds.sw.on()
+        #else:
+        #    dds.sw.off()
+
+    @kernel
+    def dds_init(self, dds_list):
+        self.core.break_realtime()
+        for dds in dds_list:
+            dds.init()
