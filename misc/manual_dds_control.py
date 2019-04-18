b8582c68c9720dcb1204cc102c6c7a62cb2a7cb5
Joseph Broz <jbroz@berkeley.edu>
2019-04-17 17:09:33 -0700

    t

--- misc/manual_dds_control.py
+++ misc/manual_dds_control.py
@@ -32,13 +32,13 @@ class change_cw(EnvExperiment):
 
     @kernel
     def run(self):
-        #self.core.reset()
-        #for cpld in self.cpld_list:
-        #    cpld.init()
+        self.core.reset()
+        for cpld in self.cpld_list:
+            cpld.init()
         self.core.break_realtime()
         with parallel:
             for i in range(len(self.dds_list)):
-                #self.dds_list[i].init()
+                self.dds_list[i].init()
                 self.dds_list[i].set(self.freq_list[i],
                                      amplitude=self.amp_list[i])
                 self.dds_list[i].set_att(self.att_list[i]*dB)
