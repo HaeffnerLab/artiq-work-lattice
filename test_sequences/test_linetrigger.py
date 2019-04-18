b37f7b7f2f6609eb6a2f491c9c520d6f5bcfd32f
Joseph Broz <jbroz@berkeley.edu>
2019-04-18 10:23:37 -0700

    t

---
+++ test_sequences/test_linetrigger.py
@@ -0,0 +1,18 @@
+from artiq import *
+from artiq.language import *
+
+class test_line_trigger(EnvExperiment):
+
+    def build(self):
+        self.setattr_device("core")
+        self.setattr_device("LTriggerIN")
+
+    def prepare(self):
+        self.set_dataset("pmt_counts", [], broadcast=True)
+
+    @kernel
+    def run(self):
+        while True:
+            while self.LTriggerIN.watch_stay_on():
+                self.append_to_dataset("pmt_counts", 1)
+
