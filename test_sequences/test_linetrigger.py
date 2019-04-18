8181387790bcfee820e480f0361bd29b18f134bd
Joseph Broz <jbroz@berkeley.edu>
2019-04-18 10:56:45 -0700

    t

--- test_sequences/test_linetrigger.py
+++ test_sequences/test_linetrigger.py
@@ -16,7 +16,7 @@ class test_line_trigger(EnvExperiment):
         while True:
             self.core.break_realtime()
             try:
-                self.LTriggerIN.watch_stay_on():
+                self.LTriggerIN.watch_stay_on()
             finally:
                 self.LTriggerIN.watch_done()
             #self.append_to_dataset("pmt_counts", 1)
