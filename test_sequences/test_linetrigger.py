03899eb8fe4b17bc1c9d5eefb30a96c18c0cc480
Joseph Broz <jbroz@berkeley.edu>
2019-04-18 10:57:50 -0700

    t

--- test_sequences/test_linetrigger.py
+++ test_sequences/test_linetrigger.py
@@ -14,7 +14,7 @@ class test_line_trigger(EnvExperiment):
     def run(self):
         self.core.reset()
         while True:
-            self.core.break_realtime()
+            #self.core.break_realtime()
             try:
                 self.LTriggerIN.watch_stay_off()
             finally:
