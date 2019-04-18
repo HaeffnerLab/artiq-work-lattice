1c797d297a2c4df9f332e4e3481b054471a9f539
Joseph Broz <jbroz@berkeley.edu>
2019-04-18 10:26:35 -0700

    t

--- test_sequences/test_linetrigger.py
+++ test_sequences/test_linetrigger.py
@@ -12,7 +12,6 @@ class test_line_trigger(EnvExperiment):
 
     @kernel
     def run(self):
-        while True:
-            while self.LTriggerIN.watch_stay_on():
-                self.append_to_dataset("pmt_counts", 1)
+        while self.LTriggerIN.watch_stay_on():
+            self.append_to_dataset("pmt_counts", 1)
 
