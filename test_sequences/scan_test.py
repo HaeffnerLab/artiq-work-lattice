601268777495da1ea605d9b36ffa5d7cb2510573
Joseph Broz <jbroz@berkeley.edu>
2019-04-17 18:05:36 -0700

    t

--- test_sequences/scan_test.py
+++ test_sequences/scan_test.py
@@ -76,11 +76,7 @@ class scanTest(EnvExperiment):
             self.amp_list.append(float(settings[1][1]))
             self.att_list.append(float(settings[1][3]))
             self.state_list.append(bool(float(settings[1][2])))
-        print(self.dds_list)
-        print(self.freq_list)
-        print(self.amp_list)
-        print(self.att_list)
-        print(self.state_list)
+       
         #------------ try to make rcg/hist connection -----------------------------
         try:
             self.rcg = Client("::1", 3286, "rcg")
