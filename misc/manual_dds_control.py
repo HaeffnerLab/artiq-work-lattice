0d98f5b4c2765aeea4095677db6c221ac14de4af
Joseph Broz <jbroz@berkeley.edu>
2019-04-17 17:04:31 -0700

    t

--- misc/manual_dds_control.py
+++ misc/manual_dds_control.py
@@ -33,8 +33,8 @@ class change_cw(EnvExperiment):
     @kernel
     def run(self):
         #self.core.reset()
-        for cpld in self.cpld_list:
-            cpld.init()
+        #for cpld in self.cpld_list:
+        #    cpld.init()
         if self.scheduler.check_pause():
             return
         self.core.break_realtime()
