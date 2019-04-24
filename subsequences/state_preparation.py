7ddddcca7edd24d95369491d17d254d468b460e5
Joseph Broz <jbroz@berkeley.edu>
2019-04-24 15:39:18 -0700

    t

--- subsequences/state_preparation.py
+++ subsequences/state_preparation.py
@@ -2,14 +2,14 @@ from artiq.subsequence import subsequence
 from artiq.experiment import *
 
 
-class StatePreparation(subsequence):
+class StatePreparation(EnvExperiment):
 
-    def build(self):
-        self.setattr_device("core")
+    #def build(self):
+    #    self.setattr_device("core")
 
-    def setup(self):
-        print("\n"*10, "something", "\n"*10)
-        #self.setattr_device("core")
+    #def setup(self):
+    #    print("\n"*10, "something", "\n"*10)
+    #    #self.setattr_device("core")
 
     @kernel
     def sequence(self):
