1d7867b6f45274106b06c314f95558eb50258dac
Joseph Broz <jbroz@berkeley.edu>
2019-04-24 15:24:24 -0700

    t

--- subsequences/state_preparation.py
+++ subsequences/state_preparation.py
@@ -5,12 +5,14 @@ from artiq.experiment import *
 class StatePreparation(subsequence):
 
     def setup(self):
-        self.setattr_device("core")
+        print("\n"*10, "soething", "\n"*10)
+        #self.setattr_device("core")
 
-    @kernel
+    #@kernel
     def sequence(self):
-        self.parent_sequence.core.break_realtime()
-        self.parent_sequence.dds_729G.sw.on()
-        delay(self.p.StateReadout.state_readout_duration)
-        self.parent_sequence.dds_729G.sw.off()
+        #self.parent_sequence.core.break_realtime()
+        #self.parent_sequence.dds_729G.sw.on()
+        #delay(self.p.StateReadout.state_readout_duration)
+        #self.parent_sequence.dds_729G.sw.off()
+        pass
 
