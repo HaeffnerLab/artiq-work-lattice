0843660d16ad5f4730ccbdb6176b8b9ebef4d80b
Joseph Broz <jbroz@berkeley.edu>
2019-04-26 17:53:52 -0700

    t

--- test_sequences/pulse_sequence_test.py
+++ test_sequences/pulse_sequence_test.py
@@ -19,7 +19,7 @@ class pstest(PulseSequence):
     def line2(self):
         print("yo")
         
-    @kernel
+    
     def foo(self, delay_):
         self.core.break_realtime()  
         self.dds_729L1.sw.on()
