3afa8df5f1960f491fa4fdc520f42e0a7a0d0f3a
Joseph Broz <jbroz@berkeley.edu>
2019-04-28 16:24:10 -0700

    t

--- test_sequences/pulse_sequence_test.py
+++ test_sequences/pulse_sequence_test.py
@@ -4,7 +4,8 @@ from artiq.experiment import *
 
 class pstest(PulseSequence):
     rcg_tab = "Rabi"
-    accessed_params = {"StateReadout.pmt_readout_duration"}
+    accessed_params = {"StateReadout.pmt_readout_duration",
+                       "Spectrum.wait_time_1"}
     accessed_params.update(StatePreparation.accessed_params)
     # fixed_params = [("StateReadout.pmt_readout_duration", 100*ms)]
     scan_params = {"line1": [("Spectrum.pulse_duration", 0, 1, 10)]}
