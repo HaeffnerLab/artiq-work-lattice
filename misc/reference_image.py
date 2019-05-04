5c9649d9bad99455847c345391dda8bc0f4caa6b
Joseph Broz <jbroz@berkeley.edu>
2019-05-04 14:17:56 -0700

    t

--- misc/reference_image.py
+++ misc/reference_image.py
@@ -62,7 +62,7 @@ class ReferenceImage(EnvExperiment):
         self.dds_854.sw.pulse(200*us)
         self.prepare_camera()
         self.core.break_realtime()
-        self.camera_ttl.pulse(self.duration * 2)
+        self.camera_ttl.pulse(self.duration) * self.N
         # for i in range(self.N * 2):
         #     # self.core.break_realtime()
         #     self.camera_ttl.pulse(self.ctw)
