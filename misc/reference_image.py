30cba73177165fdea5da1374a8ab25a93b05424f
Joseph Broz <jbroz@berkeley.edu>
2019-05-04 14:15:50 -0700

    t

--- misc/reference_image.py
+++ misc/reference_image.py
@@ -65,8 +65,8 @@ class ReferenceImage(EnvExperiment):
         for i in range(self.N * 2):
             # self.core.break_realtime()
             self.camera_ttl.pulse(self.ctw)
-            delay(self.duration)
-            # delay(self.duration + self.cta)
+            # delay(self.duration)
+            delay(self.duration + self.cta)
         self.reset_cw_settings()
 
     @kernel
