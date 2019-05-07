7be876f81628c8ff1eef1301c78622ab845fc389
Joseph Broz <jbroz@berkeley.edu>
2019-05-07 11:03:34 -0700

    t

--- test_sequences/pulse_sequence_test.py
+++ test_sequences/pulse_sequence_test.py
@@ -8,6 +8,7 @@ class pstest(PulseSequence):
                        "StateReadout.use_camera_for_readout",
                        "StateReadout.readout_mode"}
     accessed_params.update(StatePreparation.accessed_params)
+    pstest.initialize_parameters()
     # fixed_params = [("StateReadout.pmt_readout_duration", 100*ms)]
     PulseSequence.scan_params.update(line1=([("Spectrum.pulse_duration", 0, 1, 10)], "Rabi"),
                                      line2=([("Spectrum.dummy_detuning", 0, 1, 10)], "Spectrum"))
