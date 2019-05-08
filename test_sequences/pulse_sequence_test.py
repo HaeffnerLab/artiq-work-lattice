15a909ba16be36b68da9aecbff9a5582e2b280b2
Joseph Broz <jbroz@berkeley.edu>
2019-05-08 16:47:39 -0700

    t

--- test_sequences/pulse_sequence_test.py
+++ test_sequences/pulse_sequence_test.py
@@ -23,7 +23,7 @@ class pstest(PulseSequence):
     
     @kernel
     def line1(self):
-        self.sp.run(1*ms)
+        self.sp.run(1*s)
         param = self.get_variable_parameter("Spectrum_pulse_duration")*ms
         # param = self.Spectrum_wait_time_1
         self.foo(param)
