0531dcb6139c9f4ad2c02be25445737cc448145e
Joseph Broz <jbroz@berkeley.edu>
2019-05-08 16:40:25 -0700

    t

--- test_sequences/pulse_sequence_test.py
+++ test_sequences/pulse_sequence_test.py
@@ -17,9 +17,11 @@ class pstest(PulseSequence):
     PulseSequence.scan_params.update(line1=([("Spectrum.pulse_duration", 0, 1, 10)], "Rabi"),
                                      line2=([("Spectrum.dummy_detuning", 0, 1, 10)], "Spectrum"))
 
+    sp = StatePreparation()
+
     @kernel
     def line1(self):
-        StatePreparation(self).run(1*ms)
+        sp.run(1*ms)
         param = self.get_variable_parameter("Spectrum_pulse_duration")*ms
         # param = self.Spectrum_wait_time_1
         self.foo(param)
