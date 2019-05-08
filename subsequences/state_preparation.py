d31688b9ef0054d17c7c1e6fcb7604fae2b33cad
Joseph Broz <jbroz@berkeley.edu>
2019-05-08 16:29:52 -0700

    t

--- subsequences/state_preparation.py
+++ subsequences/state_preparation.py
@@ -2,7 +2,7 @@ from artiq.experiment import *
 
 
 class StatePreparation(EnvExperiment):
-    accessed_params = {"StateReadout.line_1_amplitude"}
+    accessed_params = {}#{"StateReadout.line_1_amplitude"}
 
     def build(self):
         self.setattr_device("core")
