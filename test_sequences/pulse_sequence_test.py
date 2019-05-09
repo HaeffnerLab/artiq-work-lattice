8da341787cfcba3f47d5140c2dd1cb9aa8886e8b
Joseph Broz <jbroz@berkeley.edu>
2019-05-08 19:13:46 -0700

    t

--- test_sequences/pulse_sequence_test.py
+++ test_sequences/pulse_sequence_test.py
@@ -15,7 +15,8 @@ class pstest(PulseSequence):
              }#"StateReadout.readout_mode"}
         )
     # fixed_params = [("StateReadout.pmt_readout_duration", 100*ms)]
-    PulseSequence.scan_params.update(line1=([("Spectrum.pulse_duration", 0, 1, 10)], "Rabi"),
+    PulseSequence.scan_params.update(line1=([("Spectrum.pulse_duration", 0, 1, 10),
+                                             ("Spectrum.dummy_detuning", 0, 1, 10)], "Rabi"),
                                      line2=([("Spectrum.dummy_detuning", 0, 1, 10)], "Spectrum"))
 
     def run_initially(self):
