1abc249eba4bbafa42ddb2abeb23809c273e37eb
Joseph Broz <jbroz@berkeley.edu>
2019-05-09 10:57:51 -0700

    t

--- test_sequences/pulse_sequence_test.py
+++ test_sequences/pulse_sequence_test.py
@@ -21,7 +21,7 @@ class pstest(PulseSequence):
     
     @kernel
     def line1(self):
-        self.calc_frequency("S+1/2D-3/2", 100*kHz)
+        print(self.calc_frequency("S+1/2D-3/2", 100*kHz, self.aux_axial, 1))
         self.sp.run(duration=self.Spectrum_wait_time_1)
         param = self.get_variable_parameter("Spectrum_pulse_duration")*ms
         # param = self.Spectrum_wait_time_1
