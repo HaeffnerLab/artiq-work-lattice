9a06da2963eb210195b1db55d2f02fb519709449
Joseph Broz <jbroz@berkeley.edu>
2019-05-13 14:10:18 -0700

    t

--- test_sequences/pulse_sequence_test.py
+++ test_sequences/pulse_sequence_test.py
@@ -34,20 +34,12 @@ class pstest(PulseSequence):
         opc_dds = self.opc.channel_729
         self.calc_frequency(opc_line, param, self.aux_axial, 0, opc_dds,
                 bound_param="Spectrum_dummy_detuning")
-        # opc_freq_729 = self.calc_frequency(opc_line, 0., self.aux_axial, 0, opc_dds)
-        # delay(800*us)
-        # self.repump854.duration = param*ms
-        # self.repump854.run(self)
-        # self.dopplerCooling.run(self)
-        # # self.opc.freq_729 = opc_freq_729
-        # # self.opc.run(self)
-
         opc_freq_729 =self.calc_frequency(opc_line, 0., self.aux_axial, 0, opc_dds, bound_param="")
         delay(1*ms)
         self.repump854.duration = param*ms
         self.repump854.run(self)
         self.dopplerCooling.run(self)
-        # print(self.opc.freq_729)# = opc_freq_729
+        self.opc.freq_729 = 210*MHz#opc_freq_729
         self.opc.run(self)
 
     @kernel
