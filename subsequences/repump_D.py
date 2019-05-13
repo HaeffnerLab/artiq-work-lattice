fec372c0892f51f4415549360673d0d08c5ac3d4
Joseph Broz <jbroz@berkeley.edu>
2019-05-12 20:22:56 -0700

    t

--- subsequences/repump_D.py
+++ subsequences/repump_D.py
@@ -10,29 +10,19 @@ class RepumpD(EnvExperiment):
     amplitude_866="DopplerCooling.doppler_cooling_amplitude_866"
     att_866="DopplerCooling.doppler_cooling_att_866"
     
-    def subsequence(self,
-            duration="RepumpD_5_2.repump_d_duration",
-            frequency_854="RepumpD_5_2.repump_d_frequency_854",
-            amplitude_854="RepumpD_5_2.repump_d_amplitude_854",
-            att_854="RepumpD_5_2.repump_d_att_854",
-            frequency_866="DopplerCooling.doppler_cooling_frequency_866",
-            amplitude_866="DopplerCooling.doppler_cooling_amplitude_866",
-            att_866="DopplerCooling.doppler_cooling_att_866"):
+    def subsequence(self):
 
-        # self.core.break_realtime()
-        # self.dds_854.set(frequency_854, amplitude=amplitude_854)
-        # self.dds_854.set_att(att_854)
-        # self.dds_866.set(frequency_866, amplitude=amplitude_866)
-        # self.dds_866.set_att(att_866)
-        # with parallel:
-        #     self.dds_854.sw.on()
-        #     self.dds_866.sw.on()
-        # print("DURATIN: ", duration)
+        self.dds_854.set(RepumpD.frequency_854, amplitude=RepumpD.amplitude_854)
+        self.dds_854.set_att(RepumpD.att_854)
+        self.dds_866.set(RepumpD.frequency_866, amplitude=RepumpD.amplitude_866)
+        self.dds_866.set_att(RepumpD.att_866)
+        with parallel:
+            self.dds_854.sw.on()
+            self.dds_866.sw.on()
         delay(RepumpD.duration)
-        # print("RepumpD.duration: ", RepumpD.duration)
-        # with parallel:
-        #     self.dds_854.sw.off()
-        #     self.dds_866.sw.off()
+        with parallel:
+            self.dds_854.sw.off()
+            self.dds_866.sw.off()
 
 
 
