4ab9d7469168a263bc98d6f88ecab521f56a698c
Joseph Broz <jbroz@berkeley.edu>
2019-05-12 17:45:56 -0700

    t

--- subsequences/repump_D.py
+++ subsequences/repump_D.py
@@ -2,9 +2,6 @@ from artiq.experiment import *
 from artiq.pulse_sequence import PulseSequence
 
 class RepumpD(EnvExperiment):
-
-    def build(self):
-        self.setattr_device("core")
     
     def subsequence(self,
             duration="RepumpD_5_2.repump_d_duration",
@@ -15,7 +12,7 @@ class RepumpD(EnvExperiment):
             amplitude_866="DopplerCooling.doppler_cooling_amplitude_866",
             att_866="DopplerCooling.doppler_cooling_att_866"):
 
-        self.core.break_realtime()
+        # self.core.break_realtime()
         # self.dds_854.set(frequency_854, amplitude=amplitude_854)
         # self.dds_854.set_att(att_854)
         # self.dds_866.set(frequency_866, amplitude=amplitude_866)
