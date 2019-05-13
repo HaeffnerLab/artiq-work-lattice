6df4cd8efe3e33bfb65c094f4e0256ed04361cf8
Joseph Broz <jbroz@berkeley.edu>
2019-05-13 14:10:49 -0700

    t

--- test_sequences/pulse_sequence_test.py
+++ test_sequences/pulse_sequence_test.py
@@ -39,7 +39,7 @@ class pstest(PulseSequence):
         self.repump854.duration = param*ms
         self.repump854.run(self)
         self.dopplerCooling.run(self)
-        self.opc.freq_729 = 210*MHz#opc_freq_729
+        self.opc.freq_729 = opc_freq_729
         self.opc.run(self)
 
     @kernel
