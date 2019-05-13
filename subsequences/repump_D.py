8ac95f9b4bdf854f7e557b1fc9e7ba6acc24237c
Joseph Broz <jbroz@berkeley.edu>
2019-05-12 20:38:35 -0700

    t

--- subsequences/repump_D.py
+++ subsequences/repump_D.py
@@ -13,10 +13,14 @@ class RepumpD(EnvExperiment):
     def subsequence(self):
 
         self.core.break_realtime()
-        self.dds_854.set(RepumpD.frequency_854, amplitude=RepumpD.amplitude_854)
-        self.dds_854.set_att(RepumpD.att_854)
-        self.dds_866.set(RepumpD.frequency_866, amplitude=RepumpD.amplitude_866)
-        self.dds_866.set_att(RepumpD.att_866)
+        self.dds_854.set(80.*MHz, amplitude=1.)
+        self.dds_854.set_att(13.)
+        self.dds_866.set(80.*MHz, amplitude=1.)
+        self.dds_866.set_att(13.)
+        # self.dds_854.set(RepumpD.frequency_854, amplitude=RepumpD.amplitude_854)
+        # self.dds_854.set_att(RepumpD.att_854)
+        # self.dds_866.set(RepumpD.frequency_866, amplitude=RepumpD.amplitude_866)
+        # self.dds_866.set_att(RepumpD.att_866)
         # with parallel:
         #     self.dds_854.sw.on()
         #     self.dds_866.sw.on()
