b16ff2eaf1dfcf9048082b9e5d947e07bbfa602f
Joseph Broz <jbroz@berkeley.edu>
2019-05-13 10:19:32 -0700

    t

--- test_sequences/pulse_sequence_test.py
+++ test_sequences/pulse_sequence_test.py
@@ -32,7 +32,7 @@ class pstest(PulseSequence):
                             bound_param="Spectrum_dummy_detuning")
         self.repump854.duration = param*ms
         self.repump854.run(self)
-        self.dopplerCooling.run(self, 80*MHz, 1., 13*dB, 80*MHz, 1., 13*dB, 4*ms, 100*us)
+        self.dopplerCooling.run(self)
         self.foo(1*ms)
         # self.foo(self.Spectrum_pulse_duration)
 
