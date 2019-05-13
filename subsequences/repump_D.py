b4ff28efae44e935c54d7fce7556afe5200e5e11
Joseph Broz <jbroz@berkeley.edu>
2019-05-12 20:27:49 -0700

    t

--- subsequences/repump_D.py
+++ subsequences/repump_D.py
@@ -13,6 +13,7 @@ class RepumpD(EnvExperiment):
     def subsequence(self):
 
         self.dds_854.set(RepumpD.frequency_854, amplitude=RepumpD.amplitude_854)
+        self.core.break_realtime()
         # self.dds_854.set_att(RepumpD.att_854)
         self.dds_866.set(RepumpD.frequency_866, amplitude=RepumpD.amplitude_866)
         # self.dds_866.set_att(RepumpD.att_866)
