ce950d37f6ee8dfd3870d239809208b914d0ad71
Joseph Broz <jbroz@berkeley.edu>
2019-05-13 14:54:57 -0700

    t

--- subsequences/rabi_excitation.py
+++ subsequences/rabi_excitation.py
@@ -4,7 +4,7 @@ from artiq.experiment import *
 class RabiExcitation():
     freq_729="Excitation_729.rabi_excitation_frequency"
     amp_729="Excitation_729.rabi_excitation_amplitude"
-    att_729="Excitation_729.rab_excitation_att"
+    att_729="Excitation_729.rabi_excitation_att"
     phase_729="Excitation_729.rabi_excitation_phase"
     channel_729="Excitation_729.channel_729"
     duration="Excitation_729.rabi_excitation_duration"
