d8eab587d6bb943e338fb8833f530dc6474e560f
Joseph Broz <jbroz@berkeley.edu>
2019-05-12 21:01:10 -0700

    t

--- subsequences/repump_D.py
+++ subsequences/repump_D.py
@@ -14,17 +14,17 @@ class RepumpD(EnvExperiment):
         pass
         # self.dds_854.init()
         # self.dds_866.init()
-        # self.dds_854.set(RepumpD.frequency_854, amplitude=RepumpD.amplitude_854)
-        # self.dds_854.set_att(RepumpD.att_854)
-        # self.dds_866.set(RepumpD.frequency_866, amplitude=RepumpD.amplitude_866)
-        # self.dds_866.set_att(RepumpD.att_866)
-        # with parallel:
-        #     self.dds_854.sw.on()
-        #     self.dds_866.sw.on()
-        # delay(RepumpD.duration)
-        # with parallel:
-        #     self.dds_854.sw.off()
-        #     self.dds_866.sw.off()
+        self.dds_854.set(RepumpD.frequency_854, amplitude=RepumpD.amplitude_854)
+        self.dds_854.set_att(RepumpD.att_854)
+        self.dds_866.set(RepumpD.frequency_866, amplitude=RepumpD.amplitude_866)
+        self.dds_866.set_att(RepumpD.att_866)
+        with parallel:
+            self.dds_854.sw.on()
+            self.dds_866.sw.on()
+        delay(RepumpD.duration)
+        with parallel:
+            self.dds_854.sw.off()
+            self.dds_866.sw.off()
 
 
 
