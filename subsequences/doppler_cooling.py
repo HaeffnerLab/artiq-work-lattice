76c474ad2f8a1e6cb1798c38e8d72571423cbc89
Joseph Broz <jbroz@berkeley.edu>
2019-05-13 10:18:39 -0700

    t

--- subsequences/doppler_cooling.py
+++ subsequences/doppler_cooling.py
@@ -11,16 +11,32 @@ class DopplerCooling():#EnvExperiment):
     amplitude_866="DopplerCooling.doppler_cooling_amplitude_866"
     att_866="DopplerCooling.doppler_cooling_att_866"
 
-    def subsequence(self):
+    # def subsequence(self):
+    #     delay(300*us)
+    #     self.dds_397.set(DopplerCooling.frequency_397, amplitude=DopplerCooling.amplitude_397)
+    #     self.dds_397.set_att(DopplerCooling.att_397)
+    #     self.dds_866.set(DopplerCooling.frequency_866, amplitude=DopplerCooling.amplitude_866)
+    #     self.dds_866.set_att(DopplerCooling.att_866)
+    #     with parallel:
+    #         self.dds_397.sw.on()
+    #         self.dds_866.sw.on()
+    #     delay(DopplerCooling.duration)
+    #     self.dds_397.sw.off()
+    #     delay(DopplerCooling.additional_repump_duration)
+    #     self.dds_866.sw.off()
+
+    def subsequence(self, frequency_397, amplitude_397, att_397,
+                          frequency_866, amplitude_866, att_866,
+                          duration, additional_repump_duration):
         delay(300*us)
-        self.dds_397.set(DopplerCooling.frequency_397, amplitude=DopplerCooling.amplitude_397)
-        self.dds_397.set_att(DopplerCooling.att_397)
-        self.dds_866.set(DopplerCooling.frequency_866, amplitude=DopplerCooling.amplitude_866)
-        self.dds_866.set_att(DopplerCooling.att_866)
+        self.dds_397.set(frequency_397, amplitude=amplitude_397)
+        self.dds_397.set_att(att_397)
+        self.dds_866.set(frequency_866, amplitude=amplitude_866)
+        self.dds_866.set_att(att_866)
         with parallel:
             self.dds_397.sw.on()
             self.dds_866.sw.on()
-        delay(DopplerCooling.duration)
+        delay(duration)
         self.dds_397.sw.off()
-        delay(DopplerCooling.additional_repump_duration)
+        delay(additional_repump_duration)
         self.dds_866.sw.off()
