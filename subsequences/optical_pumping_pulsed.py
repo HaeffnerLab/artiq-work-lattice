6a47b2aebc8e8f8371a7491c8cb684cce5998c30
Joseph Broz <jbroz@berkeley.edu>
2019-05-13 13:37:43 -0700

    t

---
+++ subsequences/optical_pumping_pulsed.py
@@ -0,0 +1,44 @@
+from artiq.experiment import *
+
+
+class OpticalPumpingPulsed():
+    freq_729=220*MHz
+    number_of_cycles="StatePreparation.number_of_cycles"
+    duration_854="StatePreparation.pulsed_854_duration"
+    pi_time="StatePreparation.pi_time"
+    channel_729="StatePreparation.channel_729"
+    amplitude_729="StatePreparation.pulsed_amplitude"
+    att_729="StatePreparation.pulsed_att"
+    frequency_866="DopplerCooling.doppler_cooling_frequency_866"
+    amplitude_866="DopplerCooling.doppler_cooling_amplitude_866"
+    att_866="DopplerCooling.doppler_cooling_att_866"
+    frequency_854="OpticalPumping.optical_pumping_frequency_854"
+    amplitude_854="OpticalPumping.optical_pumping_amplitude_854"
+    att_854="OpticalPumping.optical_pumping_att_854"
+    line_selection="OpticalPumping.line_selection"
+
+    def subsequence(self):
+        if OpticalPumpingPulsed.channel_729 == "729L1":
+            dds_729 = self.dds_729L1
+        elif OpticalPumpingPulsed.channel_729 == "729L2":
+            dds_729 = self.dds_729L2
+        elif OpticalPumpingPulsed.channel_729 == "729G":
+            dds_729 = self.dds_729G
+        
+        self.dds_866.set(OpticalPumpingPulsed.frequency_866, amplitude=OpticalPumpingPulsed.amplitude_866)
+        self.dds_866.set_att(OpticalPumpingPulsed.att_866)
+        dds_729.set(OpticalPumpingPulsed.freq_729, amplitude=OpticalPumpingPulsed.amplitude_729)
+        dds_729.set_att(OpticalPumpingPulsed.att_729)
+        self.dds_854.set(OpticalPumpingPulsed.frequency_854, amplitude=OpticalPumpingPulsed.amplitude_854)
+        self.dds_854.set_att(OpticalPumpingPulsed.att_854)
+        self.dds_866.sw.on()
+        for _ in range(int(OpticalPumpingPulsed.number_of_cycles)):
+            with parallel:
+                dds_729.sw.on()
+            delay(pi_time)
+            with parallel:
+                dds_729.sw.off()
+                self.dds_854.sw.on()
+            delay(OpticalPumpingPulsed.duration_854)
+            self.dds_854.sw.off()
+        self.dds_866.sw.off()
--- test_sequences/pulse_sequence_test.py
+++ test_sequences/pulse_sequence_test.py
@@ -1,6 +1,7 @@
 from artiq.pulse_sequence import PulseSequence
 from subsequences.repump_D import RepumpD
 from subsequences.doppler_cooling import DopplerCooling
+from susbsequences.optical_pumping_pulsed import OpticalPumpingPulsed
 from artiq.experiment import *
 
 
@@ -24,19 +25,22 @@ class pstest(PulseSequence):
     def run_initially(self):
         self.repump854 = self.add_subsequence(RepumpD)
         self.dopplerCooling = self.add_subsequence(DopplerCooling)
+        self.opc = self.add_subsequence(OpticalPumpingPulsed)
     
     @kernel
     def line1(self):
         param = self.get_variable_parameter("Spectrum_dummy_detuning")
-        self.calc_frequency("S+1/2D-3/2", param, self.aux_axial, 0, "729L1", 
-                            bound_param="Spectrum_dummy_detuning")
+        opc_lin3 = self.opc.line_selection
+        opc_dds = self.opc.channel_729
+        opc_freq_729 = self.calc_frequency(line, 0., self.aux_axial, 0, "729L1")
         delay(800*us)
         self.repump854.duration = param*ms
         self.repump854.run(self)
         self.dopplerCooling.run(self)
+        self.opc.freq_729 = opc_freq_729
+        self.opc.run(self)
         # self.foo(1*ms)
         # self.foo(self.Spectrum_pulse_duration)
-        # dds = self.get_device("729L1")
 
     @kernel
     def line2(self):
