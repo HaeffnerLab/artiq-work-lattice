7649e68df11d4e361d85997f58af1d6b6cad364b
Joseph Broz <jbroz@berkeley.edu>
2019-05-24 13:03:44 -0700

    t

--- calibration_scans/calib_sideband.py
+++ calibration_scans/calib_sideband.py
@@ -51,12 +51,14 @@ class CalibSideband(PulseSequence):
         self.rabi.freq_729 = self.calc_frequency(rabi_line, delta, sideband=self.sideband, order=1, 
             dds=rabi_dds, bound_param="Spectrum_sideband_detuning")
 
-        while True:
-            try:
-                self.repump854.run(self)
-                break
-            except RTIOUnderflow:
-                delay(25*us)
+        # while True:
+        #     try:
+        #         self.repump854.run(self)
+        #         break
+        #     except RTIOUnderflow:
+        #         delay(25*us)
+
+        self.repump854.run(self)
         
         self.dopplerCooling.run(self)
         self.opc.run(self)
