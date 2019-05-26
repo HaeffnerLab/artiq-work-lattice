d11b6822444756c16d66bd2ec08dd2d2d0698b48
Joseph Broz <jbroz@berkeley.edu>
2019-05-26 16:23:09 -0700

    t

--- subsequences/sideband_cooling.py
+++ subsequences/sideband_cooling.py
@@ -36,19 +36,19 @@ class SidebandCooling:
         self.dds_729.set(freq_729, amplitude=SidebandCooling.amplitude_729)
         self.dds_729.set_att(SidebandCooling.att_729)
         
-        krun(self)
+        self.krun()
         
-        if SidebandCooling.sequential_enable:
-            self.get_729_dds(SidebandCooling.sequential_channel_729)
-            krun(self)
+        # if SidebandCooling.sequential_enable:
+        #     self.get_729_dds(SidebandCooling.sequential_channel_729)
+        #     krun(self)
 
-        if SidebandCooling.sequential1_enable:
-            self.get_729_dds(SidebandCooling.sequential1_channel_729)
-            krun(self)
+        # if SidebandCooling.sequential1_enable:
+        #     self.get_729_dds(SidebandCooling.sequential1_channel_729)
+        #     krun(self)
 
-        if SidebandCooling.sequential2_enable:
-            self.get_729_dds(SidebandCooling.sequential2_channel_729)
-            krun(self)
+        # if SidebandCooling.sequential2_enable:
+        #     self.get_729_dds(SidebandCooling.sequential2_channel_729)
+        #     krun(self)
         
         
         
@@ -57,14 +57,14 @@ class SidebandCooling:
         delay(SidebandCooling.repump_additional)
         self.dds_866.sw.off()
 
-@kernel
-def krun(self):
-    with parallel:
-        self.dds_729.sw.on()
-        self.dds_729_SP.sw.on()
-        self.dds_854.sw.on()
-        self.dds_866.sw.on()
-    delay(SidebandCooling.duration)
-    with parallel:
-        self.dds_729.sw.off()
-        self.dds_729_SP.sw.off()
+    @kernel
+    def krun(self):
+        with parallel:
+            self.dds_729.sw.on()
+            self.dds_729_SP.sw.on()
+            self.dds_854.sw.on()
+            self.dds_866.sw.on()
+        delay(SidebandCooling.duration)
+        with parallel:
+            self.dds_729.sw.off()
+            self.dds_729_SP.sw.off()
