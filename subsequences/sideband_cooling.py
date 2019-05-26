c8195c0378f96f79fc9703e09c6fc567bf27fc2b
Joseph Broz <jbroz@berkeley.edu>
2019-05-26 16:31:19 -0700

    t

--- subsequences/sideband_cooling.py
+++ subsequences/sideband_cooling.py
@@ -49,7 +49,7 @@ class SidebandCooling:
         # if SidebandCooling.sequential2_enable:
         #     self.get_729_dds(SidebandCooling.sequential2_channel_729)
         #     krun(self)
-        
+        #
         
         
         delay(SidebandCooling.repump_additional)
