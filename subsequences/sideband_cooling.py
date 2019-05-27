6d918db0b6913495f22d69760bb6af3ad14a79cc
Joseph Broz <jbroz@berkeley.edu>
2019-05-26 19:07:30 -0700

    t

--- subsequences/sideband_cooling.py
+++ subsequences/sideband_cooling.py
@@ -37,9 +37,9 @@ class SidebandCooling:
                     order=SidebandCooling.order,
                     dds=SidebandCooling.channel_729)
         self.dds_729.set(freq_729, amplitude=1.)#SidebandCooling.amplitude_729)
-        self.dds_729.set_att(10*dB)#SidebandCooling.att_729)#
+        self.dds_729.set_att(10*dB)#SidebandCooling.att_729)
         
-        # krun(self)#
+        # krun(self)
         
         # if SidebandCooling.sequential_enable:
         #     self.get_729_dds(SidebandCooling.sequential_channel_729)
