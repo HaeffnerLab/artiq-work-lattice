7b3b184c061318adf67928d953101dd701fdc68b
Joseph Broz <jbroz@berkeley.edu>
2019-05-27 16:09:12 -0700

    t

--- misc/set_dopplercooling_and_statereadout.py
+++ misc/set_dopplercooling_and_statereadout.py
@@ -89,7 +89,7 @@ class set_dopplercooling_and_statereadout(EnvExperiment):
         self.turn_off_all()
         delay(2*ms)
         t_count = self.pmt.gate_rising(self.readout_duration*10)
-        self.background_level = self.pmt.count(t_count) / 10
+        self.background_level = round(self.pmt.count(t_count) / 10)
         delay(2*ms)
         self.dds_866.set(self.freq_866*MHz, amplitude=self.amp_866)
         self.dds_866.set_att(self.att_866*dB)
