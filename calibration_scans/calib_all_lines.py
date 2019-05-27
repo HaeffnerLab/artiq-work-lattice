eb88598d3c0a7732a097c9b52667f38207b92f70
Joseph Broz <jbroz@berkeley.edu>
2019-05-26 19:28:45 -0700

    t

--- calibration_scans/calib_all_lines.py
+++ calibration_scans/calib_all_lines.py
@@ -29,8 +29,8 @@ class CalibAllLines(PulseSequence):
 
     fixed_params = [("Display.relative_frequencies", False)]
 
-    # PulseSequence.scan_params["CalibLine2"] = ("CalibLine2",
-    #             [("Spectrum.carrier_detuning", -5e3, 5e3, 15, "kHz")])
+    PulseSequence.scan_params["CalibLine2"] = ("CalibLine2",
+                [("Spectrum.carrier_detuning", -5e3, 5e3, 15, "kHz")])
     PulseSequence.scan_params["CalibLine1"] = ("CalibLine1",
                 [("Spectrum.carrier_detuning", -5e3, 5e3, 15, "kHz")])
 
@@ -90,7 +90,7 @@ class CalibAllLines(PulseSequence):
     
     @kernel
     def CalibLine2(self):
-        # delay(1*ms)
+        delay(1*ms)
         self.repump854.run(self)
         self.dopplerCooling.run(self)
         self.opc.run(self)
