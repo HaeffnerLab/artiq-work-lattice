c9fe00974f6655106074e18cd094c2a4d7b6d202
Joseph Broz <jbroz@berkeley.edu>
2019-05-27 15:30:10 -0700

    t

--- misc/set_dopplercooling_and_statereadout.py
+++ misc/set_dopplercooling_and_statereadout.py
@@ -61,7 +61,7 @@ class set_dopplercooling_and_statereadout(EnvExperiment):
         self.dataset_length = dict()
 
     def run(self):
-        self.initialize()
+        self.initialize(self.freq_866, self.amp_866, self.att_866, self.amp_397, self.att_397)
         
         freq_list = np.linspace(65*MHz, 85*MHz, self.scan_length)
         self.scan_freq_list = freq_list
@@ -85,18 +85,17 @@ class set_dopplercooling_and_statereadout(EnvExperiment):
         self.reset_cw_settings()
 
     @kernel
-    def initialize(self):
+    def initialize(self, freq_866, amp_866, att_866, amp_397, att_397):
         self.turn_off_all()
         self.core.reset()
         t_count = self.pmt.gate_rising(self.readout_duration)
         self.background_level = self.pmt.count(t_count)
         self.core.break_realtime()
-        self.dds_866.set(self.freq_866*MHz, amplitude=self.amp_866)
-        self.core.break_realtime()
-        self.dds_866.set_att(self.att_866*dB)
+        self.dds_866.set(freq_866*MHz, amplitude=amp_866)
+        self.dds_866.set_att(att_866*dB)
         self.dds_866.sw.on()
         self.dds_397.set(65*MHz, amplitude=0.2)
-        self.dds_397.set_att(self.att_397*dB)
+        self.dds_397.set_att(att_397*dB)
         self.dds_397.sw.on()
 
     @kernel
