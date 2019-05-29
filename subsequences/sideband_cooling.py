9c63fe54491970704bda63aa681ebc67ce2c12f4
Joseph Broz <jbroz@berkeley.edu>
2019-05-29 16:16:44 -0700

    debug camera

--- subsequences/sideband_cooling.py
+++ subsequences/sideband_cooling.py
@@ -112,6 +112,6 @@ class SidebandCooling:
                 self.dds_729.sw.off()
                 self.dds_729_SP.sw.off()
         
-        delay(3. * SidebandCooling.repump_additional)
+        delay(SidebandCooling.repump_additional)
         # self.dds_854.sw.off()
         # self.dds_866.sw.off()
