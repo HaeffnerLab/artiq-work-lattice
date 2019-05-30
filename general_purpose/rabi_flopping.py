6c0a22038a8d19e1ff86b0903cf1ae3014b475e5
Joseph Broz <jbroz@berkeley.edu>
2019-05-29 18:06:46 -0700

    debug camera

--- general_purpose/rabi_flopping.py
+++ general_purpose/rabi_flopping.py
@@ -54,5 +54,6 @@ class RabiFlopping(PulseSequence):
         self.opc.run(self)
         if self.StatePreparation_sideband_cooling_enable:
             self.sbc.run(self)
+        self.core.wait_until_mu(now_mu())
         self.rabi.run(self)
         
