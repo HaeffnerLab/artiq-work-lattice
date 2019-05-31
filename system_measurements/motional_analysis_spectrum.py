b593c4a362da921cec08822720b8a5070cafb81e
Joseph Broz <jbroz@berkeley.edu>
2019-05-30 22:29:01 -0700

    debug ma

--- system_measurements/motional_analysis_spectrum.py
+++ system_measurements/motional_analysis_spectrum.py
@@ -60,7 +60,7 @@ class MotionalAnalysisSpectrum(PulseSequence):
         self.detuning = self.sideband + self.get_variable_parameter("MotionAnalysis_detuning")
         self.n = int(self.detuning * self.MotionAnalysis_pulse_width_397)
         print("N: ", self.n)
-        self.n = 1
+        self.n = 10
         self.duration = .5 / self.detuning
         self.amp_397 = self.get_variable_parameter("MotionAnalysis_amplitude_397")
 
