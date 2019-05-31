84fbcd1b751ca297eee10afbf8c1f28fc28a87c3
Joseph Broz <jbroz@berkeley.edu>
2019-05-30 21:31:38 -0700

    added motionalspectrum

--- subsequences/motional_analysis.py
+++ subsequences/motional_analysis.py
@@ -3,15 +3,15 @@ from artiq.experiment import *
 
 class MotionalAnalysis:
     freq_397="DopplerCooling.doppler_cooling_frequency_397"
-    amp_397="Motion_Analysis.amplitude_397"
-    att_397="Motion_Analysis.att_397"
+    amp_397="MotionAnalysis.amplitude_397"
+    att_397="MotionAnalysis.att_397"
 
     freq_866="DopplerCooling.doppler_cooling_frequency_866"
     amp_866="DopplerCooling.doppler_cooling_amplitude_866"
     att_866="DopplerCooling.doppler_cooling_att_866"
 
-    pulse_width="Motion_Analysis.pulse_width_397"
-    sideband=1*MHz
+    pulse_width="MotionAnalysis.pulse_width_397"
+    detuning=1*MHz
 
 
     def subsequence(self):
@@ -22,8 +22,8 @@ class MotionalAnalysis:
                          amplitude=MotionalAnalysis.amp_866)
         self.dds_866.set_att(MotionalAnalysis.att_866)
         self.dds_866.sw.on()
-        n = int(self.calculate_number_of_pulses(MotionalAnalysis., MotionalAnalysis.pulse_width))
-        duration = 1 / sideband
+        n = int(self.calculate_number_of_pulses(MotionalAnalysis.detuning, MotionalAnalysis.pulse_width))
+        duration = 1 / MotionalAnalysis.detuning
         self.record(n, duration)
         pulses_handle = self.core_dma.get_handle("pulses")
         self.core_dma.playback_handle(pulses_handle)
---
+++ system_measurements/motional_analysis_spectrum.py
@@ -0,0 +1,65 @@
+from pulse_sequence import PulseSequence
+from subsequences.repump_D import RepumpD
+from subsequences.doppler_cooling import DopplerCooling
+from subsequences.optical_pumping_pulsed import OpticalPumpingPulsed
+from subsequences.rabi_excitation import RabiExcitation
+from subsequences.sideband_cooling import SidebandCooling
+from subsequences.motional_analysis import MotionalAnalysis
+from artiq.experiment import *
+
+
+class MotionalAnalysisSpectrum(PulseSequence):
+    PulseSequence.accessed_params.update(
+        {"MotionAnalysis.pulse_width_397",
+         "MotionAnalysis.amplitude_397",
+         "MotionAnalysis.sideband_selection",
+         "RabiFlopping.duration",
+         "RabiFlopping.rabi_amplitude_729",
+         "RabiFlopping.line_selection",
+         "RabiFlopping.selection_sideband",
+         "RabiFlopping.att_729",
+         "RabiFlopping.order"}
+    )
+
+    PulseSequence.scan_params.update(
+        MotionalSpectrum=("Spectrum",
+        [("MotionAnalysis.detuning", 0., 50e3, 25, "kHz"),
+         ("MotionAnalysis.amplitude_397", 0., 1., 25)])
+    )
+
+    def run_initially(self):
+        self.repump854 = self.add_subsequence(RepumpD)
+        self.dopplerCooling = self.add_subsequence(DopplerCooling)
+        self.opc = self.add_subsequence(OpticalPumpingPulsed)
+        self.sbc = self.add_subsequence(SidebandCooling)
+        self.rabi = self.add_subsequence(RabiExcitation)
+        self.motional_analysis = self.add_subsequence(MotionalAnalysis)
+        self.set_subsequence["MotionalSpectrum"] = self.set_subsequence_motionalspectrum
+        self.sideband = self.p.TrapFrequencies[self.p.RabiFlopping.selection_sideband]
+
+    @kernel
+    def set_subsequence_motionalspectrum(self):
+        self.rabi.duration = self.RabiFlopping_duration
+        self.rabi.amp_729 = self.RabiFlopping_amplitude_729
+        self.rabi.att_729 = self.RabiFlopping_att_729
+        self.rabi.freq_729 = self.calc_frequency(
+            self.RabiFlopping_line_selection, 
+            sideband=self.RabiFlopping_selection_sideband,
+            order=self.RabiFlopping_order, 
+            dds=self.RabiFlopping_channel_729
+        )
+        self.motional_analysis.amplitude_397 = self.get_variable_parameter("MotionAnalysis_amplitude_397")
+        self.motional_analysis.detuning = self.sideband + self.get_variable_parameter("MotionAnalysis_detuning")
+
+    @kernel
+    def MotionalSpectrum(self):
+        delay(1*ms)
+        self.repump854.run(self)
+        self.dopplerCooling.run(self)
+        self.opc.run(self)
+        if self.StatePreparation_sideband_cooling_enable:
+            self.sbc.run(self)
+            self.opc.run(self)
+        self.motional_analysis.run(self)
+        self.opc.run(self)
+        self.rabi.run(self)
