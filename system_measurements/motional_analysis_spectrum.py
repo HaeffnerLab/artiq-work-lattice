667213de86b30365d8e2829ac332219d8c1f4777
Joseph Broz <jbroz@berkeley.edu>
2019-05-31 00:10:36 -0700

    debug ma

--- system_measurements/motional_analysis_spectrum.py
+++ system_measurements/motional_analysis_spectrum.py
@@ -85,6 +85,6 @@ class MotionalAnalysisSpectrum(PulseSequence):
             delay(self.duration)
         self.core.wait_until_mu(now_mu())
         self.dds_866.sw.off
-        self.opc.run(self)
+        # self.opc.run(self)
 
-        self.rabi.run(self)
+        # self.rabi.run(self)
