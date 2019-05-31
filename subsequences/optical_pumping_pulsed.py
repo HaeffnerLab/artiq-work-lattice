f937d8b1a7fb4aed091254ab3f73e6fc92d207fa
Joseph Broz <jbroz@berkeley.edu>
2019-05-31 01:08:31 -0700

    debug ma

--- subsequences/optical_pumping_pulsed.py
+++ subsequences/optical_pumping_pulsed.py
@@ -41,7 +41,6 @@ class OpticalPumpingPulsed:
                 self.dds_729_SP.sw.off()
                 self.dds_854.sw.on()
                 self.dds_866.sw.on()
-            self.core.wait_until_mu(now_mu() + self.core.seconds_to_mu(1000*us))
-            delay(OpticalPumpingPulsed.duration_854)
+            delay(OpticalPumpingPulsed.duration_854 + 1*ms)
             self.dds_854.sw.off()
             self.dds_866.sw.off()
