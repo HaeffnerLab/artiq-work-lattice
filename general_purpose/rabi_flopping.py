acb719c9977bc6ba15a59bac67f40693c2bce101
Joseph Broz <jbroz@berkeley.edu>
2019-06-01 03:50:18 -0700

    ditto

--- general_purpose/rabi_flopping.py
+++ general_purpose/rabi_flopping.py
@@ -1,6 +1,7 @@
 from pulse_sequence import PulseSequence
 from subsequences.doppler_cooling import DopplerCooling
 from subsequences.optical_pumping_pulsed import OpticalPumpingPulsed
+from subsequences.optical_pumping_continuous import OpticalPumpingContinuous
 from subsequences.rabi_excitation import RabiExcitation
 from subsequences.sideband_cooling import SidebandCooling
 from artiq.experiment import *
@@ -26,7 +27,8 @@ class RabiFlopping(PulseSequence):
 
     def run_initially(self):
         self.dopplerCooling = self.add_subsequence(DopplerCooling)
-        self.opc = self.add_subsequence(OpticalPumpingPulsed)
+        self.opp = self.add_subsequence(OpticalPumpingPulsed)
+        self.opc = self.add_subsequence(OpticalPumpingContinuous)
         self.sbc = self.add_subsequence(SidebandCooling)
         self.rabi = self.add_subsequence(RabiExcitation)
         self.set_subsequence["RabiFlopping"] = self.set_subsequence_rabiflopping
@@ -48,9 +50,13 @@ class RabiFlopping(PulseSequence):
     def RabiFlopping(self):
         delay(1*ms)
         self.dopplerCooling.run(self)
-        self.opc.run(self)
+        if self.StatePreparation_pulsed_optical_pumping:
+            self.opp.run(self)
+        else:
+            self.opc.run(self)
         if self.StatePreparation_sideband_cooling_enable:
             self.sbc.run(self)
+            self.opc.duration = 100*us
             self.opc.run(self)
         self.rabi.run(self)
         
