ff38e1ac99060b488e6b8fbe0fdef581ba526250
Joseph Broz <jbroz@berkeley.edu>
2019-05-31 23:41:09 -0700

    debug rdt

--- calibration_scans/ramsey_drift_tracker.py
+++ calibration_scans/ramsey_drift_tracker.py
@@ -205,7 +205,7 @@ class RamseyDriftTracker(PulseSequence):
         cxn.disconnect()
 
     def run_finally(self):
-
+        
         line1 = self.p.DriftTracker.line_selection_1
         line2 = self.p.DriftTracker.line_selection_2
 
@@ -216,15 +216,16 @@ class RamseyDriftTracker(PulseSequence):
         carr2 = self.detuning_2_global * 1e-6 + old_carr2
 
         submission = [(line1, U(carr1, "MHz")), (line2, U(carr2, "MHz"))]
+        print("SUBMISSION: ", submission)
 
-        try:
-            global_cxn = labrad.connect(cl.global_address, password=cl.global_password,
-                                        tls_mode="off")
-            global_cxn.sd_tracker_global.set_measurements(submission, cl.client_name)
-        except:
-            logger.error("Failed to connect to global drift tracker.", exc_info=True)
-            return
-        global_cxn.disconnect()
+        # try:
+        #     global_cxn = labrad.connect(cl.global_address, password=cl.global_password,
+        #                                 tls_mode="off")
+        #     global_cxn.sd_tracker_global.set_measurements(submission, cl.client_name)
+        # except:
+        #     logger.error("Failed to connect to global drift tracker.", exc_info=True)
+        #     return
+        # global_cxn.disconnect()
 
 
 
