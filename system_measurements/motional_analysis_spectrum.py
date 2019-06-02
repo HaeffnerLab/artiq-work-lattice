d87efe97fb95a805988aac7a57b420fc133557ee
Joseph Broz <jbroz@berkeley.edu>
2019-06-01 18:20:04 -0700

    debug maspec

--- system_measurements/motional_analysis_spectrum.py
+++ system_measurements/motional_analysis_spectrum.py
@@ -32,7 +32,7 @@ class MotionalAnalysisSpectrum(PulseSequence):
     PulseSequence.scan_params.update(
         MotionalSpectrum=[
             ("Spectrum", ("MotionAnalysis.detuning", 0., 50e3, 25, "kHz")),
-            ("Current", ("MotionAnalysis.amplitude_397", 0., 1., 25))
+            ("Current", ("MotionAnalysis.amplitude_397", 0., 1., 25))#
         ]
     )
 
