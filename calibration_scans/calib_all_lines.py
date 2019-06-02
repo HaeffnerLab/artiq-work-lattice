f1b23f71ca27c3703dea7b8b0c588096e5788e98
Joseph Broz <jbroz@berkeley.edu>
2019-06-01 18:24:38 -0700

    new fmt caliballlines

--- calibration_scans/calib_all_lines.py
+++ calibration_scans/calib_all_lines.py
@@ -34,10 +34,10 @@ class CalibAllLines(PulseSequence):
     fixed_params = [("Display.relative_frequencies", False),
                     ("StateReadout.readout_mode", "pmt")]
 
-    PulseSequence.scan_params["CalibLine2"] = ("CalibLine2",
-            [("Spectrum.carrier_detuning", -5e3, 5e3, 15, "kHz")])
-    PulseSequence.scan_params["CalibLine1"] = ("CalibLine1",
-            [("Spectrum.carrier_detuning", -5e3, 5e3, 15, "kHz")])
+    PulseSequence.scan_params["CalibLine2"] =
+            [("CalibLine2", ("Spectrum.carrier_detuning", -5e3, 5e3, 15, "kHz"))]
+    PulseSequence.scan_params["CalibLine1"] =
+            [("CalibLine1", ("Spectrum.carrier_detuning", -5e3, 5e3, 15, "kHz"))]
 
 
     def run_initially(self):
