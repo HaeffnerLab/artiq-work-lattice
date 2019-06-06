8ee4b83bb91aa3966aaa854ce55effc2f635234c
Joseph Broz <jbroz@berkeley.edu>
2019-06-05 17:40:55 -0700

    debug optimize sbc

--- calibration_scans/optimize_sideband_cooling.py
+++ calibration_scans/optimize_sideband_cooling.py
@@ -67,10 +67,12 @@ class OptimizeSidebandCooling(PulseSequence):
         self.dopplerCooling.run(self)
         if self.StatePreparation_pulsed_optical_pumping:
             self.opp.run(self)
-        else:
+        elif self.StatePreparation_optical_pumping_enable:
             self.opc.run(self)
         if self.StatePreparation_sideband_cooling_enable:
             self.sbc.run(self)
-            self.opc.duration = 100*us
-            self.opc.run(self)
+            if self.StatePreparation_pulsed_optical_pumping:
+                self.opp.run(self)
+            elif self.StatePreparation_optical_pumping_enable:
+                self.opc.run(self)
         self.rabi.run(self)
