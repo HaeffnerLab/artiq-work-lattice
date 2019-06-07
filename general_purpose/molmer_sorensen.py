79731667d7e8617d4a3f27c8a30dd5827ff4c49d
Joseph Broz <jbroz@berkeley.edu>
2019-06-07 14:02:17 -0700

    debug ms

--- general_purpose/molmer_sorensen.py
+++ general_purpose/molmer_sorensen.py
@@ -15,7 +15,7 @@ class MolmerSorensenGate(PulseSequence):
         "MolmerSorensen.line_selection",
         "MolmerSorensen.line_selection_ion2",
         "MolmerSorensen.due_carrier_enable",
-        "MolmerSorensen.sideband_selection",
+        "MolmerSorensen.selection_sideband",
         "MolmerSorensen.detuning",
         "MolmerSorensen.amp_red",
         "MolmerSorensen.att_red",
