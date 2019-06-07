b844bde13b3702380625d474bb49f4bf1767d169
Joseph Broz <jbroz@berkeley.edu>
2019-06-07 15:41:15 -0700

    set single pass params explicitly in rabiexcitation

--- subsequences/rabi_excitation.py
+++ subsequences/rabi_excitation.py
@@ -16,6 +16,8 @@ class RabiExcitation:
         self.dds_729.set(r.freq_729, amplitude=r.amp_729,
                     phase=r.phase_729 / 360)
         self.dds_729.set_att(r.att_729)
+        self.dds_729_SP.set(80*MHz, amplitude=1.)
+        self.dds_729_SP.set_att(4*dB)
         with parallel:
             self.dds_729.sw.on()
             self.dds_729_SP.sw.on()
@@ -23,4 +25,3 @@ class RabiExcitation:
         with parallel:
             self.dds_729.sw.off()
             self.dds_729_SP.sw.off()
-#
