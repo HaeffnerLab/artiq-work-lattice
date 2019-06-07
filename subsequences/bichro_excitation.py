91bda31969e3dfe981bf28091366a429c5e3f5cb
Joseph Broz <jbroz@berkeley.edu>
2019-06-07 12:01:15 -0700

    debug ms

--- subsequences/bichro_excitation.py
+++ subsequences/bichro_excitation.py
@@ -32,6 +32,7 @@ class BichroExcitation:
         trap_frequency = self.get_trap_frequency(b.selection_sideband)
         freq_blue = 80*MHz + trap_frequency + b.detuning
         freq_red = 80*MHz - trap_frequency - b.detuning
+        print("FREQS: ", freq_red, freq_blue)
         if b.channel == "global":
             self.get_729_dds("729G")
             dp_freq = self.calc_frequency(
