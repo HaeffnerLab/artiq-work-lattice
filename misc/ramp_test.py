fee462b0d69d5a185b83defcd22ecb30c125dcf3
Joseph Broz <jbroz@berkeley.edu>
2019-06-07 17:23:54 -0700

    test ramp

--- misc/ramp_test.py
+++ misc/ramp_test.py
@@ -1,4 +1,5 @@
 from artiq.experiment import *
+from artiq.coredevice.ad9910 import RAM_MODE_BIDIR_RAMP, RAM_DEST_ASF
 
 
 class RampTest(EnvExperiment):
@@ -15,7 +16,21 @@ class RampTest(EnvExperiment):
         self.dds.init()
         self.core.break_realtime()
 
-        self.dds.set(80*MHz, amplitude=1., profile=5)
+        n = 10
+        data = [0]*(1 << n)
+        for i in range(len(data)//2):
+            data[i] = i << (32 - (n - 1))
+            data[i + len(data)//2] = 0xffff << 16
+
+        self.dds.cpld.set_profile_ram(
+                start=0, end=0 + len(data) - 1, step=1,
+                profile=0, mode=RAM_MODE_BIDIR_RAMP
+            )
+        self.dds.cpld.set_profile(0)
+        self.dds.write_ram(data)
+        self.dds.set_cfr1(ram_enable=1, ram_destination=RAM_DEST_ASF)
+
+        self.dds.set(80*MHz, amplitude=1., profile=0)
         self.dds.set_att(5*dB)
 
         self.dds.sw.on()
