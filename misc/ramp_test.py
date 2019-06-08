153fbdac0c02f1045015a82ff5d27d3eb492a5b7
Joseph Broz <jbroz@berkeley.edu>
2019-06-07 17:08:15 -0700

    testing ramp

--- misc/ramp_test.py
+++ misc/ramp_test.py
@@ -5,7 +5,7 @@ class RampTest(EnvExperiment):
 
     def build(self):
         self.setattr_device("core")
-        self.dds = self.get_device("SP_729L1")
+        self.dds = self.get_device("SP_729L2")
         self.cpld = self.get_device("urukul2_cpld")
 
     @kernel
@@ -15,7 +15,7 @@ class RampTest(EnvExperiment):
         self.dds.init()
         self.core.break_realtime()
 
-        self.dds.set(80*MHz, amplitude=1)
+        self.dds.set(80*MHz, amplitude=1.)
         self.dds.set_att(5*dB)
 
         self.dds.sw.on()
