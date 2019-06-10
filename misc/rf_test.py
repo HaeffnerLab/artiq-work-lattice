6a8dce23797c21caa9912cdc272013d3c0384542
Joseph Broz <jbroz@berkeley.edu>
2019-06-10 12:18:31 -0700

    re_test

---
+++ misc/rf_test.py
@@ -0,0 +1,17 @@
+from artiq.experiment import *
+
+
+class rf_test(EnvExperiment):
+    def build(self):
+        self.setattr_device("core")
+        self.dds = self.get_device("729G")
+
+    @kernel
+    def run(self):
+        self.core.reset()
+        self.dds.set(220*MHz, amplitude=1.)
+        self.dds.set_att(20*dB)
+        self.core.break_realtime()
+        self.dds.sw.on()
+        delay(10000*s)
+        self.dds.sw.off()
