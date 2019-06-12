a37b81d1ab9b7585f187c0735614f4c723d946ef
Joseph Broz <jbroz@berkeley.edu>
2019-06-12 16:25:10 -0700

    debug

--- subsequences/optical_pumping_pulsed.py
+++ subsequences/optical_pumping_pulsed.py
@@ -44,10 +44,10 @@ class OpticalPumpingPulsed:
                 self.dds_866.sw.on()
             if i != int(o.number_of_cycles) - 1:
                 delay(o.duration_854)
+                delay(20*us)
             else:
                 delay(10*us)
                 self.dds_854.set(80*MHz, amplitude=1.)
                 delay(50*us)
-            delay(10*us)
             self.dds_854.sw.off()
             self.dds_866.sw.off()
