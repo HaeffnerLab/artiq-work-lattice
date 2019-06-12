f9d80d5bee15c52a42565a471821d400394953bc
Joseph Broz <jbroz@berkeley.edu>
2019-06-12 16:17:17 -0700

    debug

--- subsequences/optical_pumping_pulsed.py
+++ subsequences/optical_pumping_pulsed.py
@@ -46,9 +46,8 @@ class OpticalPumpingPulsed:
             if i != int(o.number_of_cycles) - 1:
                 delay(o.duration_854)
             else:
-                self.core.wait_until_mu(now_mu())
-                delay(10*us)
                 self.dds_854.set(80*MHz, amplitude=1.)
+                self.core.wait_until_mu(now_mu())
             delay(1000*us)
             self.dds_854.sw.off()
             self.dds_866.sw.off()
