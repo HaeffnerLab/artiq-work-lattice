e04a2aecc3e3eaac076879d8cb1e731fc2237112
Joseph Broz <jbroz@berkeley.edu>
2019-07-05 14:21:24 -0700

    dds

--- subsequences/optical_pumping_pulsed.py
+++ subsequences/optical_pumping_pulsed.py
@@ -17,7 +17,7 @@ class OpticalPumpingPulsed:
 
     def subsequence(self):
         o = OpticalPumpingPulsed
-        self.get_729_dds(o.channel_729)
+        self.get_729_dds(o.channel_729, "OpticalPumping")
         self.dds_866.set(o.frequency_866, 
                          amplitude=o.amplitude_866)
         self.dds_866.set_att(o.att_866)
@@ -25,20 +25,20 @@ class OpticalPumpingPulsed:
             o.line_selection,
             dds=o.channel_729
         )
-        self.dds_729.set(freq_729, 
+        self.dds_729_OP.set(freq_729, 
                          amplitude=o.amplitude_729)
-        self.dds_729.set_att(o.att_729)
+        self.dds_729_OP.set_att(o.att_729)
         self.dds_854.set(o.frequency_854, 
                          amplitude=o.amplitude_854)
         self.dds_854.set_att(o.att_854)
         for i in range(int(o.number_of_cycles)):
             with parallel:
-                self.dds_729.sw.on()
-                self.dds_729_SP.sw.on()
+                self.dds_729_OP.sw.on()
+                self.dds_729_SP_OP.sw.on()
             delay(o.pi_time)
             with parallel:
-                self.dds_729.sw.off()
-                self.dds_729_SP.sw.off()
+                self.dds_729_OP.sw.off()
+                self.dds_729_SP_OP.sw.off()
                 self.dds_854.sw.on()
                 self.dds_866.sw.on()
             if i != int(o.number_of_cycles) - 1:
