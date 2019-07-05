148b64216071ce857a93fe9dd141d271933442a5
Joseph Broz <jbroz@berkeley.edu>
2019-07-05 15:54:29 -0700

    dds

--- general_purpose/rabi_flopping.py
+++ general_purpose/rabi_flopping.py
@@ -30,7 +30,7 @@ class RabiFlopping(PulseSequence):
         self.opc = self.add_subsequence(OpticalPumpingContinuous)
         self.sbc = self.add_subsequence(SidebandCooling)
         self.rabi = self.add_subsequence(RabiExcitation)
-        self.rabi.channel_729 = "729L1" #self.p.RabiFlopping.channel_729
+        self.rabi.channel_729 = self.p.RabiFlopping.channel_729
         self.set_subsequence["RabiFlopping"] = self.set_subsequence_rabiflopping
 
     @kernel
