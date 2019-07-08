6dd9eabc349b6f29ddd3166844ef208ef59f8641
Joseph Broz <jbroz@berkeley.edu>
2019-07-08 10:07:05 -0700

    dds

--- subsequences/rabi_excitation.py
+++ subsequences/rabi_excitation.py
@@ -1,5 +1,6 @@
 from artiq.experiment import *
 
+
 class RabiExcitation:
     freq_729="Excitation_729.rabi_excitation_frequency"
     amp_729="Excitation_729.rabi_excitation_amplitude"
@@ -16,10 +17,10 @@ class RabiExcitation:
                     phase=r.phase_729 / 360)
         self.dds_729.set_att(r.att_729)
         
-        #with parallel:
-        self.dds_729.sw.on()
-        self.dds_729_SP.sw.on()
+        with parallel:
+            self.dds_729.sw.on()
+            self.dds_729_SP.sw.on()
         delay(r.duration)
-        #with parallel:
-        self.dds_729.sw.off()
-        self.dds_729_SP.sw.off()
+        with parallel:
+            self.dds_729.sw.off()
+            self.dds_729_SP.sw.off()
