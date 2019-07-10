6f514630c665296f5f1a2f430d94e20e10d027b6
Joseph Broz <jbroz@berkeley.edu>
2019-07-10 14:45:59 -0700

    state prep

--- subsequences/state_preparation.py
+++ subsequences/state_preparation.py
@@ -9,29 +9,30 @@ class StatePreparation:
     enable_pulsed_optical_pumping="StatePreparation.pulsed_optical_pumping"
     enable_sideband_cooling="StatePreparation.sideband_cooling_enable"
     sideband_cooling_cycles="SidebandCooling.sideband_cooling_cycles"
-    
-    def add_child_subsequences(self, pulse_sequence):
-        self.dopplerCooling = pulse_sequence.add_subsequence(DopplerCooling)
-        self.opp = pulse_sequence.add_subsequence(OpticalPumpingPulsed)
-        self.opc = pulse_sequence.add_subsequence(OpticalPumpingContinuous)
-        self.sbc = pulse_sequence.add_subsequence(SidebandCooling)
+
+    def add_child_subsequences(pulse_sequence):
+        s = StatePreparation
+        s.dopplerCooling = pulse_sequence.add_subsequence(DopplerCooling)
+        s.opp = pulse_sequence.add_subsequence(OpticalPumpingPulsed)
+        s.opc = pulse_sequence.add_subsequence(OpticalPumpingContinuous)
+        s.sbc = pulse_sequence.add_subsequence(SidebandCooling)
 
     @kernel
     def subsequence(self):
         s = StatePreparation
 
         delay(1*ms)
-        self.dopplerCooling.run(self)
+        s.dopplerCooling.run(self)
         if s.enable_pulsed_optical_pumping:
-            self.opp.run(self)
+            s.opp.run(self)
         elif s.enable_optical_pumping:
-            self.opc.run(self)
+            s.opc.run(self)
 
         if s.enable_sideband_cooling:
             num_cycles = int(s.sideband_cooling_cycles)
             for i in range(num_cycles):
-                self.sbc.run(self)
+                s.sbc.run(self)
                 if s.enable_pulsed_optical_pumping:
-                    self.opp.run(self)
+                    s.opp.run(self)
                 elif s.enable_optical_pumping:
-                    self.opc.run(self)
+                    s.opc.run(self)
