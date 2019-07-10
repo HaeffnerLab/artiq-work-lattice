e2229bb27d5ad524b6538d7a88b21452ef88fce8
Joseph Broz <jbroz@berkeley.edu>
2019-07-09 17:00:24 -0700

    dds

--- subsequences/rabi_excitation.py
+++ subsequences/rabi_excitation.py
@@ -17,8 +17,8 @@ class RabiExcitation:
         self.dds_729.set(r.freq_729, amplitude=r.amp_729,
                     phase=r.phase_729 / 360)
         self.dds_729.set_att(r.att_729)
-        #self.dds_729_SP.set_amplitude(r.sp_amp_729)
-        #self.dds_729_SP.set_att(r.sp_att_729)
+        self.dds_729_SP.set_amplitude(r.sp_amp_729)
+        self.dds_729_SP.set_att(r.sp_att_729)
         
         with parallel:
             self.dds_729.sw.on()
@@ -26,4 +26,4 @@ class RabiExcitation:
         delay(r.duration)
         with parallel:
             self.dds_729.sw.off()
-            #self.dds_729_SP.sw.off()
+            self.dds_729_SP.sw.off()
