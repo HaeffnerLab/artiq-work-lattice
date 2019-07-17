ce044a6736137e57d37b7ef4352d038a6e2c915c
Joseph Broz <jbroz@berkeley.edu>
2019-07-16 17:21:30 -0700

    ramp

--- subsequences/bichro_excitation.py
+++ subsequences/bichro_excitation.py
@@ -52,21 +52,21 @@ class BichroExcitation:
                 amps = [(1./n_steps) * b.amp_blue * (i+1) for i in range(n_steps)]
                 write = [0]*n_steps
 
-                self.dds_729_SP.amplitude_to_ram(amps, write)
+                #self.dds_729_SP.amplitude_to_ram(amps, write)
                 #print(write)
                 #self.core.break_realtime()
                 #self.dds_729_SP.set_cfr1(ram_enable=0)
                 #self.dds_729_SP.cpld.io_update.pulse_mu(8)
-                self.dds_729_SP.set_profile_ram(
-                        start=200, end=200 + n_steps - 1, step=1,
-                        profile=1, mode=RAM_MODE_RAMPUP)
-                self.dds_729_SP.cpld.set_profile(1)
-                self.dds_729_SP.cpld.io_update.pulse_mu(8)
-                delay(1*ms)
-                self.dds_729_SP.write_ram(write)
-                self.dds_729_SP.set_cfr1(ram_enable=1, ram_destination=RAM_DEST_ASF)
-                self.dds_729_SP.cpld.io_update.pulse_mu(8)
-                delay(1*ms)
+                #self.dds_729_SP.set_profile_ram(
+                #        start=200, end=200 + n_steps - 1, step=1,
+                #        profile=1, mode=RAM_MODE_RAMPUP)
+                #self.dds_729_SP.cpld.set_profile(1)
+                #self.dds_729_SP.cpld.io_update.pulse_mu(8)
+                #delay(1*ms)
+                #self.dds_729_SP.write_ram(write)
+                #self.dds_729_SP.set_cfr1(ram_enable=1, ram_destination=RAM_DEST_ASF)
+                #self.dds_729_SP.cpld.io_update.pulse_mu(8)
+                #delay(1*ms)
                 # END TEMP ramping stuff
 
                 self.dds_729_SP.set(freq_blue, amplitude=b.amp_blue)
