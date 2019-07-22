386f890fe76b848924eeeebeb0c9bf24849242a8
Joseph Broz <jbroz@berkeley.edu>
2019-07-22 16:32:12 -0700

    phase

--- general_purpose/molmer_sorensen.py
+++ general_purpose/molmer_sorensen.py
@@ -99,25 +99,19 @@ class MolmerSorensenGate(PulseSequence):
             delay(self.get_variable_parameter("MolmerSorensen_ramsey_duration"))
             #self.rabi.run(self)
 
-            # Below runs analysis pulse on blue sideband
             self.get_729_dds(self.rabi.channel_729)
             self.dds_729.set_phase_mode(PHASE_MODE_ABSOLUTE)
             self.dds_729.set(self.rabi.freq_729,
                             amplitude=self.rabi.amp_729,
                             phase=self.get_variable_parameter("MolmerSorensen_ms_phase") / 360.)
-                            #ref_time_mu=self.rabi.phase_ref_time)
-            #self.dds_729.set_att(self.ms.att)
 
-            #trap_frequency = self.get_trap_frequency(self.ms.selection_sideband)
             freq_729_SP = 80*MHz + self.get_offset_frequency(self.rabi.channel_729)
             self.dds_729_SP.set_phase_mode(PHASE_MODE_ABSOLUTE)
-            self.dds_729_SP.set(freq_729_SP, phase=0.)  #, amplitude=self.ms.amp_blue,
-                                #phase_mode=PHASE_MODE_TRACKING, ref_time_mu=self.rabi.phase_ref_time)
-            #self.dds_729_SP.set_att(self.ms.att_blue)
+            self.dds_729_SP.set(freq_729_SP, phase=0.)
             
             with parallel:
                 self.dds_729.sw.on()
-                #self.dds_729_SP.sw.on()
+                self.dds_729_SP.sw.on()
             delay(self.rabi.duration)
             with parallel:
                 self.dds_729.sw.off()
