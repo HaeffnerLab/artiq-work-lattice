affc6664160e519445ca66acd1d9ea30d967bfc1
Joseph Broz <jbroz@berkeley.edu>
2019-07-22 10:51:17 -0700

    phase

--- general_purpose/molmer_sorensen.py
+++ general_purpose/molmer_sorensen.py
@@ -70,9 +70,9 @@ class MolmerSorensenGate(PulseSequence):
 
     @kernel
     def set_subsequence_ms(self):
-        phase_ref_time = now_mu()
-        self.ms.phase_ref_time = phase_ref_time
-        self.rabi.phase_ref_time = phase_ref_time
+        self.phase_ref_time = now_mu()
+        self.ms.phase_ref_time = self.phase_ref_time
+        self.rabi.phase_ref_time = self.phase_ref_time
 
         self.ms.duration = self.get_variable_parameter("MolmerSorensen_duration")
         self.ms.amp = self.get_variable_parameter("MolmerSorensen_amplitude")
