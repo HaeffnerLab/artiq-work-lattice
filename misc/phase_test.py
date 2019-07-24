a0341a69b3fe085f2f6e59215edb9314636ca725
Joseph Broz <jbroz@berkeley.edu>
2019-07-24 16:13:46 -0700

    phase

--- misc/phase_test.py
+++ misc/phase_test.py
@@ -11,52 +11,48 @@ class phase_test(EnvExperiment):
 
     @kernel
     def run(self):
-        try:
-            self.core.reset()
-
-            self.dds1.set_phase_mode(PHASE_MODE_TRACKING)
-            self.dds2.set_phase_mode(PHASE_MODE_TRACKING)
-            self.dds_sum.set_phase_mode(PHASE_MODE_TRACKING)
-
-            ref_time = now_mu()
-            print(ref_time)
-            self.core.break_realtime()
-
-            delay(100*us)
-
-            self.dds1.set(25*MHz, ref_time_mu=ref_time)
-            self.dds1.set_att(5*dB)
-
-            self.core.break_realtime()
-
-            self.dds2.set(15*MHz, ref_time_mu=ref_time)
-            self.dds2.set_att(5*dB)
-
-            # turn on the two frequencies
-            self.dds1.sw.on()
-            self.dds2.sw.on()
-
-            delay(10*us)
-
-            self.dds_sum.set(20*MHz, ref_time_mu=ref_time)
-            self.dds_sum.set_att(5*dB)
-
-            self.core.break_realtime()
-
-            delay(10*us)
-
-            self.dds_sum.set(40*MHz, ref_time_mu=ref_time)
-            self.dds_sum.set_att(5*dB)
-
-            self.core.break_realtime()
-
-            self.dds_sum.sw.on()
-
-            delay(1000000*ms)
-
-        except:
-            pass
+        self.core.reset()
 
         self.dds1.sw.off()
         self.dds2.sw.off()
         self.dds_sum.sw.off()
+
+        self.dds1.set_phase_mode(PHASE_MODE_TRACKING)
+        self.dds2.set_phase_mode(PHASE_MODE_TRACKING)
+        self.dds_sum.set_phase_mode(PHASE_MODE_TRACKING)
+
+        ref_time = now_mu()
+        print(ref_time)
+        self.core.break_realtime()
+
+        delay(100*us)
+
+        self.dds1.set(25*MHz, ref_time_mu=ref_time)
+        self.dds1.set_att(5*dB)
+
+        self.core.break_realtime()
+
+        self.dds2.set(25*MHz, ref_time_mu=ref_time)
+        self.dds2.set_att(5*dB)
+
+        # turn on the two frequencies
+        self.dds1.sw.on()
+        self.dds2.sw.on()
+
+        # delay(10*us)
+
+        # self.dds_sum.set(20*MHz, ref_time_mu=ref_time)
+        # self.dds_sum.set_att(5*dB)
+
+        # self.core.break_realtime()
+
+        # delay(10*us)
+
+        # self.dds_sum.set(40*MHz, ref_time_mu=ref_time)
+        # self.dds_sum.set_att(5*dB)
+
+        # self.core.break_realtime()
+
+        # self.dds_sum.sw.on()
+
+        # leave each DDS on so that we can measure
