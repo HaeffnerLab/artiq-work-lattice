59e92e96af43038bef28b5d5351d75ed0b131063
Joseph Broz <jbroz@berkeley.edu>
2019-07-24 13:50:55 -0700

    test

--- misc/rf_test.py
+++ misc/rf_test.py
@@ -1,6 +1,6 @@
 from artiq.experiment import *
 
-from artiq.coredevice.ad9910 import PHASE_MODE_TRACKING
+from artiq.coredevice.ad9910 import PHASE_MODE_TRACKING, PHASE_MODE_ABSOLUTE
 
 class rf_test(EnvExperiment):
     def build(self):
@@ -12,11 +12,12 @@ class rf_test(EnvExperiment):
     def run(self):
         self.core.reset()
 
-        self.dds.set_phase_mode(PHASE_MODE_TRACKING)
+        self.dds.set_phase_mode(PHASE_MODE_ABSOLUTE)
+        self.dds2.set_phase_mode(PHASE_MODE_ABSOLUTE)
 
-        ref_time = now_mu()
+        #ref_time = now_mu()
 
-        self.dds.set(2*MHz, amplitude=1., ref_time_mu=ref_time)
+        self.dds.set(2*MHz, amplitude=1.) #, ref_time_mu=ref_time)
         self.dds.set_att(5*dB)
 
         self.core.break_realtime()
@@ -33,12 +34,14 @@ class rf_test(EnvExperiment):
         delay(2*us)
         self.dds.sw.off()
 
+        self.dds.set_phase_mode(PHASE_MODE_TRACKING)
+
         self.dds.set(10*MHz, amplitude=1.)
         self.dds.sw.on()
         delay(0.5*us)
         self.dds.sw.off()
 
-        self.dds.set(2*MHz, amplitude=1., ref_time_mu=ref_time)
+        self.dds.set(2*MHz, amplitude=1.) #, ref_time_mu=ref_time)
         self.dds.sw.on()
         delay(2*us)
 
