b59fd16c07dc8b493dd7316b8ae762d977fc00db
Joseph Broz <jbroz@berkeley.edu>
2019-07-24 15:23:13 -0700

    phase

--- misc/rf_test.py
+++ misc/rf_test.py
@@ -15,7 +15,9 @@ class rf_test(EnvExperiment):
         self.dds.set_phase_mode(PHASE_MODE_TRACKING)
         self.dds2.set_phase_mode(PHASE_MODE_TRACKING)
 
-        ref_time = 0 #now_mu()
+        ref_time = now_mu()
+        print(ref_time)
+        self.core.break_realtime()
 
         delay(100*us)
 
