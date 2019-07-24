2955c484fbc8b3e8417fa1169e5cc6062817b71a
Joseph Broz <jbroz@berkeley.edu>
2019-07-24 16:22:53 -0700

    phase

--- misc/phase_test.py
+++ misc/phase_test.py
@@ -32,7 +32,7 @@ class phase_test(EnvExperiment):
 
         self.core.break_realtime()
 
-        self.dds2.set(25*MHz+76.2*Hz, ref_time_mu=ref_time)
+        self.dds2.set(25*MHz+76.8*Hz, ref_time_mu=ref_time)
         self.dds2.set_att(5*dB)
 
         # turn on the two frequencies
