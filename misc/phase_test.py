082381a5a71f4c92465780796f2167350446e6b2
Joseph Broz <jbroz@berkeley.edu>
2019-07-25 16:58:46 -0700

    phase

--- misc/phase_test.py
+++ misc/phase_test.py
@@ -98,7 +98,7 @@ class phase_test(EnvExperiment):
         # on the third channel, first set to the bichro, then change to the default
         self.dds_sum.set(bichro_blue_freq*2., ref_time_mu=ref_time)
         self.core.break_realtime()
-        delay(100*us)
+        delay(150*us)
         self.dds_sum.set_att(5*dB)
         self.core.break_realtime()
         delay(100*us)
