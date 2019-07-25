efd86aca516f008ceeecc940116f803a9e54cc90
Joseph Broz <jbroz@berkeley.edu>
2019-07-25 16:49:41 -0700

    phase

--- misc/phase_test.py
+++ misc/phase_test.py
@@ -105,7 +105,9 @@ class phase_test(EnvExperiment):
         self.dds_sum.sw.off()
 
         phase_degrees = 0.
-        self.dds_sum.set(sp_freq*2., ref_time_mu=ref_time, phase=phase_degrees/360.)
+        pow = self.dds_sum.set(sp_freq*2., ref_time_mu=ref_time, phase=phase_degrees/360.)
+        print("phase offset:", pow)
+        self.core.break_realtime()
         self.dds_sum.set_att(5*dB)
 
         self.core.break_realtime()
