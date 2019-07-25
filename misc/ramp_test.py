1ddaab8211e23383422504adba597f5adbd771b5
Joseph Broz <jbroz@berkeley.edu>
2019-07-25 14:55:02 -0700

    ramp

--- misc/ramp_test.py
+++ misc/ramp_test.py
@@ -1,5 +1,5 @@
 from artiq.experiment import *
-from artiq.coredevice.ad9910 import RAM_MODE_BIDIR_RAMP, RAM_DEST_ASF
+from artiq.coredevice.ad9910 import RAM_MODE_BIDIR_RAMP, RAM_MODE_RAMPUP, RAM_DEST_ASF
 
 
 class RampTest(EnvExperiment):
