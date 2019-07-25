4bc1ee3f3eb61efc3ecd4c63a27683aa9b86c4b4
Joseph Broz <jbroz@berkeley.edu>
2019-07-25 15:32:41 -0700

    ramp

--- misc/ramp_test.py
+++ misc/ramp_test.py
@@ -36,7 +36,7 @@ class RampTest(EnvExperiment):
         self.core.break_realtime()
 
         self.dds.set_profile_ram(
-               start=0, end=n_steps - 1, step=10,
+               start=0, end=n_steps - 1, step=100,
                profile=0, mode=RAM_MODE_RAMPUP)
         self.dds.cpld.set_profile(0)
         #self.dds.cpld.io_update.pulse_mu(8)
