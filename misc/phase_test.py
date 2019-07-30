862a5899b3594539bf692d01a62f8205b6563a99
Joseph Broz <jbroz@berkeley.edu>
2019-07-30 08:39:53 -0700

    phase

--- misc/phase_test.py
+++ misc/phase_test.py
@@ -35,7 +35,7 @@ class phase_test(EnvExperiment):
         # The rest is happening at each future init() of the DDS.
         if True: # self.dds1.sync_delay_seed == -1:
             delay(100*us)
-            d0, w0 = self.dds1.tune_sync_delay()
+            d0, w0 = self.dds1.tune_sync_delay(search_seed=8)
             t0 = self.dds1.tune_io_update_delay()
             d1, w1 = self.dds2.tune_sync_delay()
             t1 = self.dds2.tune_io_update_delay()
