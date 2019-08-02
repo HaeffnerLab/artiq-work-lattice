12e246c772c912e6307cea2d0de089e67e231513
Joseph Broz <jbroz@berkeley.edu>
2019-08-02 10:24:31 -0700

    ramp

--- misc/ramp_test.py
+++ misc/ramp_test.py
@@ -76,8 +76,9 @@ class RampTest(EnvExperiment):
         self.dds.write_ram(data)
 
         # turn on the DDS and enable the ramp
+        self.dds.set(80.3*MHz, amplitude=1., profile=0)
         self.dds.sw.on()
-        self.dds.set(80.3*MHz, amplitude=0., profile=0)
+        delay(5*us)
 
         self.dds.set_cfr1(ram_enable=1,
             ram_destination=RAM_DEST_ASF)
