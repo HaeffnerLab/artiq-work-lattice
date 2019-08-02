0bc1960be0030aa0263cb090fd8ad15581760310
Joseph Broz <jbroz@berkeley.edu>
2019-08-02 12:38:47 -0700

    ramp

--- misc/ramp_test.py
+++ misc/ramp_test.py
@@ -101,11 +101,11 @@ class RampTest(EnvExperiment):
         self.dds.write_ram(data)
         
         #self.dds.set(80.3*MHz, amplitude=1., profile=0)
+        self.dds.set_frequency(80.3*MHz)
+        self.dds.sw.on()
 
         self.dds.set_cfr1(ram_enable=1, ram_destination=RAM_DEST_ASF)
         self.dds.cpld.io_update.pulse(1*us)
-        self.dds.set_frequency(80.3*MHz)
-        self.dds.sw.on()
 
         #self.dds.set(80.3*MHz, amplitude=1., profile=ram_profile,
         #             phase_mode=PHASE_MODE_CONTINUOUS)
