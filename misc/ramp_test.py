93551b341832999f72c8c73dce1e6ee6b700dea4
Joseph Broz <jbroz@berkeley.edu>
2019-08-02 10:17:39 -0700

    ramp

--- misc/ramp_test.py
+++ misc/ramp_test.py
@@ -18,27 +18,18 @@ class RampTest(EnvExperiment):
         self.dds.init()
         self.core.break_realtime()
 
-        n_steps = 10
-        amps = [1./n_steps * i for i in range(1, n_steps+1)]
-        data = [0]*n_steps
-        self.dds.amplitude_to_ram(amps, data)
-        # or - calculate manually:
-        #for i in range(len(amps)):
-        #    data[i] = self.dds.amplitude_to_asf(amps[i])
-
-        # freqs = [1*MHz, 5*MHz, 20*MHz, 40*MHz, 80*MHz] #[40*MHz + ((80*MHz/n_steps) * i) for i in range(i, n_steps+1)]
-        # n_steps = len(freqs)
-        # self.dds.frequency_to_ram(freqs, data)
-
-        # print(data)
-        # self.core.break_realtime()
-
+        # start by disabling ramping
         self.dds.set_cfr1(ram_enable=0)
         self.dds.cpld.io_update.pulse_mu(8)
 
+        #
+        # first, try some naive ramping.
+        # each set() call takes a little over 1us,
+        # so this isn't fast enough for us.
+        #
         self.dds.set(80.3*MHz, amplitude=0., profile=0)
         self.dds.set_att(8*dB)
-        
+
         self.dds.sw.on()
 
         self.dds.set(80.3*MHz, amplitude=0.2)
@@ -47,26 +38,53 @@ class RampTest(EnvExperiment):
         self.dds.set(80.3*MHz, amplitude=0.8)
         self.dds.set(80.3*MHz, amplitude=1.)
 
-        # start_address = 200
-        # delay(1*ms)
-        # self.dds.set_profile_ram(
-        #        start=start_address, end=start_address + n_steps - 1,
-        #        step=1, nodwell_high=0,
-        #        profile=0, mode=RAM_MODE_RAMPUP)
-        # delay(1*ms)
-        # self.dds.cpld.set_profile(0)
-        # self.dds.cpld.io_update.pulse_mu(8)
-        # delay(1*ms)
-        # self.dds.write_ram(data)
+        delay(5*us)
+        self.dds.sw.off()
 
-        # self.dds.set_cfr1(ram_enable=1,
-        #     ram_destination=RAM_DEST_ASF)
-        # self.dds.cpld.io_update.pulse_mu(8)
+        #
+        # now, try to use built-in ramping
+        # on the AD9910
+        #
+        n_steps = 10
+        amps = [1./n_steps * i for i in range(1, n_steps+1)]
+        data = [0]*n_steps
+        #self.dds.amplitude_to_ram(amps, data)
+        # or - calculating manually seems to work better:
+        for i in range(len(amps)):
+            weird_constant = 0.1
+            data[i] = weird_constant * self.dds.amplitude_to_asf(amps[i])
+
+        # freqs = [1*MHz, 5*MHz, 20*MHz, 40*MHz, 80*MHz] #[40*MHz + ((80*MHz/n_steps) * i) for i in range(i, n_steps+1)]
+        # n_steps = len(freqs)
+        # self.dds.frequency_to_ram(freqs, data)
+
+        print(data)
+        self.core.break_realtime()
+
+        start_address = 100
+        delay(1*ms)
+        self.dds.set_profile_ram(
+               start=start_address, end=start_address + n_steps - 1,
+               step=1, nodwell_high=0,
+               profile=0, mode=RAM_MODE_RAMPUP)
+        delay(1*ms)
+        self.dds.cpld.set_profile(0)
+        self.dds.cpld.io_update.pulse_mu(8)
+        delay(1*ms)
+        self.dds.write_ram(data)
+
+        self.dds.set_cfr1(ram_enable=1,
+            ram_destination=RAM_DEST_ASF)
+        self.dds.cpld.io_update.pulse_mu(8)
         #delay(1*ms)
 
-        #self.dds.set(80.3*MHz, amplitude=1., profile=0)
+        self.dds.sw.on()
 
-        delay(1000*ms)
+        self.dds.set(80.3*MHz, amplitude=0., profile=0)
+        delay(10*us)
+        self.dds.set(80.3*MHz, amplitude=1., profile=0)
+
+        delay(10*us)
         self.dds.sw.off()
 
         # self.dds.set_cfr1(ram_enable=0)
