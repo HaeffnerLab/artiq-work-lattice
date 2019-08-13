f32ff43bb843840061061c32abe13ffb4e672ceb
Joseph Broz <jbroz@berkeley.edu>
2019-08-13 16:05:49 -0700

    noise

--- subsequences/bichro_excitation.py
+++ subsequences/bichro_excitation.py
@@ -65,10 +65,6 @@ class BichroExcitation:
                 self.dds_729_SP_bichro.set(freq_red, amplitude=0., ref_time_mu=b.phase_ref_time)
                 self.dds_729_SP_bichro.set_att(b.att_red)
 
-                # with parallel:
-                #     self.dds_729_SP.sw.on()
-                #     self.dds_729_SP_bichro.sw.on()
-
                 self.start_noisy_bichro(
                     freq_blue, b.amp_blue, b.att_blue,
                     freq_red, b.amp_red, b.att_red,
@@ -80,9 +76,6 @@ class BichroExcitation:
 
                 self.stop_noisy_bichro()
 
-                # with parallel:
-                #     self.dds_729_SP.sw.off()
-                #     self.dds_729_SP_bichro.sw.off()
             else:
                 # bichro disabled
                 self.dds_729.set_amplitude(b.amp)
