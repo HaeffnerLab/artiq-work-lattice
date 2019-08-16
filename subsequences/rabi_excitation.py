e372afec8731a9799e628c28dd12fb5842efe089
Joseph Broz <jbroz@berkeley.edu>
2019-08-16 15:55:56 -0700

    ramp

--- subsequences/rabi_excitation.py
+++ subsequences/rabi_excitation.py
@@ -21,7 +21,7 @@ class RabiExcitation:
         self.prepare_pulse_with_amplitude_ramp(
             pulse_duration=r.duration,
             ramp_duration=1*us,
-            dds1_amp=r.amp_729
+            dds1_amp=r.amp_729)
         r.use_ramping = True
 
     def subsequence(self):
