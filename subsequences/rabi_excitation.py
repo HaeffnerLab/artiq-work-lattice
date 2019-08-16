a232dd22a890042d5c5d0e5672b3fd71f907970d
Joseph Broz <jbroz@berkeley.edu>
2019-08-16 15:25:51 -0700

    ramp

--- subsequences/rabi_excitation.py
+++ subsequences/rabi_excitation.py
@@ -21,7 +21,7 @@ class RabiExcitation:
                          #amplitude=r.amp_729,
                          amplitude=0.,
                          ref_time_mu=r.phase_ref_time)
-        self.dds_729.set_att(r.att_729)
+        #self.dds_729.set_att(r.att_729)
         sp_freq_729 = 80*MHz + self.get_offset_frequency(r.channel_729)
         self.dds_729_SP.set(sp_freq_729, amplitude=r.sp_amp_729, 
                          phase=r.phase_729 / 360., ref_time_mu=r.phase_ref_time)
