2db5b18aa1c11c32c1fb18ef0ac0b38f66702291
Joseph Broz <jbroz@berkeley.edu>
2019-09-28 18:01:34 -0700

    analog

--- benchmarking/analog_rb.py
+++ benchmarking/analog_rb.py
@@ -118,7 +118,7 @@ class AnalogRB(PulseSequence):
             sequence_length = len(self.sequences_reverse_step[self.sequence_number])
             for i in range(sequence_length):
                 # TODO_RYAN: Optimize or move this delay so it doesn't interrupt the pulse sequence
-                delay(50*us)
+                delay(80*us)
                 self.simulation.reverse = self.sequences_reverse_step[self.sequence_number][i]
                 self.simulation.disable_coupling_term = not self.sequences_enable_0[self.sequence_number][i]
                 self.simulation.disable_transverse_term = not self.sequences_enable_1[self.sequence_number][i]
