2f5e9bb090bab8afe7c5d6b2947104fc0d4d4e2f
berkeleyions <haeffner-lab@berkeley.edu>
2020-02-25 10:01:06 -0800

    szx

--- subsequences/szx.py
+++ subsequences/szx.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class SZX:
     bichro_enable = "SZX.bichro_enable"
-    nu_effective = "SZX.nu-effective"
+    nu_effective = "SZX.nu_effective"
     amp_blue = "SZX.amp_blue"
     amp_red = "SZX.amp_red"
     att_blue = "SZX.att_blue"
@@ -12,7 +12,7 @@ class SZX:
     car_amp_L1 = "SZX.local_rabi_amp"
     car_att = "SZX.att"
     car_att_L1 = "SZX.local_rabi_att"
-    sideband = "SZX.sideband_selection"
+    sideband_selection = "SZX.sideband_selection"
     line_selection = "SZX.line_selection"
     carrier_detuning = "SZX.carrier_detuning"
     carrier_detuning_L1 = "SZX.carrier_detuning_L1"
@@ -20,13 +20,14 @@ class SZX:
     phase_ref_time=np.int64(-1)
     sp_amp_729L1="Excitation_729.single_pass_amplitude"
     sp_att_729L1="Excitation_729.single_pass_att"
-
+    AC_stark_local="SZX.AC_stark_local"
+    use_ramping=False
 
     def subsequence(self):
         s = SZX
         trap_frequency = self.get_trap_frequency(s.sideband_selection)
-        freq_red = 80*MHz - trap_frequency*0.5 - b.detuning
-        freq_blue = 80*MHz + trap_frequency*0.5 + b.detuning + s.nu_effective
+        freq_red = 80*MHz - trap_frequency*0.5 
+        freq_blue = 80*MHz + trap_frequency*0.5 + s.nu_effective
         if s.channel == "729global":
             offset = self.get_offset_frequency("729G")
             freq_blue += offset
@@ -43,6 +44,8 @@ class SZX:
                 detuning=s.carrier_detuning,
                 dds="729G"
             )
+
+            # amplitude need to start from 0 when ramping is on
             self.dds_729.set(dp_freq,
                 amplitude=0.,
                 phase=s.phase / 360,
@@ -62,13 +65,13 @@ class SZX:
 
                 if s.AC_stark_local:
                     offset_L1 = self.get_offset_frequency("729L1")
-                    dp_freq = self.calc_frequency(
+                    dp_freq2 = self.calc_frequency(
                         s.line_selection,
                         detuning=s.carrier_detuning_L1,
                         dds="729L1"
                     )
-                    self.dds_729L1.set(dp_freq,
-                        amplitude=0.,
+                    self.dds_729L1.set(dp_freq2,
+                        amplitude=s.car_amp_L1,
                         phase=s.phase / 360,
                         ref_time_mu=s.phase_ref_time)
                     
@@ -83,11 +86,13 @@ class SZX:
 
                     with parallel:
                         self.dds_729.sw.on()
+                        self.dds_729_SP.sw.on()
                         self.dds_729L1.sw.on()
                         self.dds_729L1_SP.sw.on()
                     delay(s.duration)
                     with parallel:
                         self.dds_729.sw.off()
+                        #self.dds_729.sw.off()
                         self.dds_729L1.sw.off()
                         self.dds_729L1_SP.sw.off()
 
@@ -149,18 +154,18 @@ class SZX:
                 self.dds_729_SP_bichro.set(freq_red, amplitude=s.amp_red, ref_time_mu=s.phase_ref_time)
                 self.dds_729_SP_bichro.set_att(s.att_red)
 
-                self.start_noisy_single_pass(b.phase_ref_time,
-                    freq_noise=b.use_single_pass_freq_noise,
-                    freq_sp=freq_blue, amp_sp=b.amp_blue, att_sp=b.att_blue,
+                self.start_noisy_single_pass(s.phase_ref_time,
+                    freq_noise=s.use_single_pass_freq_noise,
+                    freq_sp=freq_blue, amp_sp=s.amp_blue, att_sp=s.att_blue,
                     use_bichro=True,
-                    freq_sp_bichro=freq_red, amp_sp_bichro=b.amp_red, att_sp_bichro=b.att_red)
+                    freq_sp_bichro=freq_red, amp_sp_bichro=s.amp_red, att_sp_bichro=s.att_red)
 
                 if b.use_ramping:
                     self.execute_pulse_with_amplitude_ramp(
-                        dds1_att=b.att,
+                        dds1_att=s.att,
                         dds1_freq=dp_freq)
                 else:
-                    self.dds_729.set(dp_freq,
+                    self.dds_729.set(dp_freq2,
                         amplitude=b.amp,
                         phase=b.phase / 360,
                         ref_time_mu=b.phase_ref_time)
