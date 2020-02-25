1812069453b746d13601d7d2748486ca8080219e
berkeleyions <haeffner-lab@berkeley.edu>
2020-02-24 18:05:30 -0800

    vaet_test

--- vaet/vaet.py
+++ vaet/vaet.py
@@ -67,12 +67,56 @@ class VAET(PulseSequence):
         self.stateprep = self.add_subsequence(StatePreparation)
         self.ms = self.add_subsequence(BichroExcitation)
         self.rabi = self.add_subsequence(RabiExcitation)
+        self.szx = self.add_subsequence(SZX)
         self.set_subsequence["VAET"] = self.set_subsequence_vaet
 
     @kernel
     def set_subsequence_vaet(self):
-        pass
+        self.vaet.duration = self.get_variable_parameter("VAET_vaet_duration")
+        self.vaet.rabi_duration = self.get_variable_parameter("VAET_rabi_duration")
+        self.szx.nu_effective = self.get_variable_parameter("SZX_nu_effective")
+        self.szx.carrier_detuning = self.get_variable_parameter("SZX_carrier_detuning")
+        self.vaet.parity = self.get_variable_parameter("VAET_ramsey_time")
+
+        
+        # "SZX.amplitude",
+        # "SZX.att",
+        # "SZX.amp_blue",
+        # "SZX.att_blue",
+        # "SZX.amp_red",
+        # "SZX.att_red",
+        # "SZX.nu_effective",
+        # "SZX.carrier_detuning",
+        # "SZX.post_local_rotation",
+        # "SZX.calibration_offset",
+        # "SZX.local_rabi_amp",
+        # "SZX.local_rabi_att",
+        # "MolmerSorensen.detuning",
+        # "MolmerSorensen.amp_red",
+        # "MolmerSorensen.att_red",
+        # "MolmerSorensen.amp_blue",
+        # "MolmerSorensen.att_blue",
+        # "MolmerSorensen.amplitude",
+        # "MolmerSorensen.att",
+        # "MolmerSorensen.amplitude_ion2",
+        # "MolmerSorensen.att_ion2",
+        # "MolmerSorensen.analysis_pulse_enable",
+        # "MolmerSorensen.SDDS_enable",
+        # "MolmerSorensen.SDDS_rotate_out",
+        # "MolmerSorensen.bichro_enable",
+        # "MolmerSorensen.analysis_duration",
+        # "MolmerSorensen.analysis_amplitude",
+        # "MolmerSorensen.analysis_att",
+        # "MolmerSorensen.analysis_amplitude_ion2",
+        # "MolmerSorensen.analysis_att_ion2",
+        # "MolmerSorensen.detuning_carrier_1",
+        # "MolmerSorensen.detuning_carrier_2",
+        # "MolmerSorensen.ramsey_duration",
+        # "VAET.ramsey_time"
+        
 
     @kernel
     def VAET(self):
         self.stateprep.run(self)
+        self.szx.run(self)
+        
