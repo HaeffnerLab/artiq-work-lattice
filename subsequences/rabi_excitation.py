b90b5d0c9170c4183c1ffcaf4f25195bbf577c7f
berkeleyions <haeffner-lab@berkeley.edu>
2020-03-04 20:50:50 -0800

    asdfasf

--- subsequences/rabi_excitation.py
+++ subsequences/rabi_excitation.py
@@ -57,7 +57,7 @@ class RabiExcitation:
             with parallel:
                 self.dds_729.sw.on()
                 self.dds_729_SP.sw.on()
-            delay_mu(r.duation * 1000)
+            delay_mu(seconds_to_mu(r.duration, self.core))
             with parallel:
                 self.dds_729.sw.off()
                 #self.dds_729_SP.sw.off()
