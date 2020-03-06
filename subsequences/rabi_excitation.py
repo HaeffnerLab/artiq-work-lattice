822bca4b59406a67b0842faeed341618fbc65ac6
berkeleyions <haeffner-lab@berkeley.edu>
2020-03-06 15:16:06 -0800

    ramp

--- subsequences/rabi_excitation.py
+++ subsequences/rabi_excitation.py
@@ -25,7 +25,7 @@ class RabiExcitation:
             pulse_duration=r.duration,
             ramp_duration=100*us,
             dds1_amp=r.amp_729)
-        r.use_ramping = True
+        r.use_ramping = False
 
     def subsequence(self):
         r = RabiExcitation
