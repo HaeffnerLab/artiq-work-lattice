896437b2594f8a469f7ebcc9ea0251dd03a2b85d
berkeleyions <haeffner-lab@berkeley.edu>
2020-03-06 13:40:40 -0800

    ramping

--- subsequences/rabi_excitation.py
+++ subsequences/rabi_excitation.py
@@ -52,7 +52,7 @@ class RabiExcitation:
             self.execute_pulse_with_amplitude_ramp(
                 dds1_att=r.att_729,
                 dds1_freq=r.freq_729)
-            # self.dds_729_SP.sw.off()
+            self.dds_729_SP.sw.off()
         else:
             with parallel:
                 self.dds_729.sw.on()
