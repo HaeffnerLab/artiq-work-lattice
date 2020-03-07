1869d71673d25e963cc07a919c2f7a8409fac3c4
berkeleyions <haeffner-lab@berkeley.edu>
2020-03-06 16:21:15 -0800

    ramp

--- subsequences/bichro_excitation.py
+++ subsequences/bichro_excitation.py
@@ -75,9 +75,9 @@ class BichroExcitation:
         b.use_ramping = True
         pulse_sequence.prepare_pulse_with_amplitude_ramp(
             pulse_duration=b.duration,
-            ramp_duration=5*us,
+            ramp_duration=3*us,
             dds1_amp=b.amp)
-        pulse_sequence.prepare_noisy_single_pass(freq_noise=b.use_single_pass_freq_noise)    
+        #pulse_sequence.prepare_noisy_single_pass(freq_noise=b.use_single_pass_freq_noise)    
     
 
     def subsequence(self):
