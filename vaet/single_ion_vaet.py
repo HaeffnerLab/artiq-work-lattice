4938fbf135a2336b3d23e8e9abc57a77dbb5d520
berkeleyions <haeffner-lab@berkeley.edu>
2020-03-13 14:20:50 -0700

    tshoot

--- vaet/single_ion_vaet.py
+++ vaet/single_ion_vaet.py
@@ -61,8 +61,9 @@ class SingleIonVAET(PulseSequence):
         self.stateprep.run(self)
         if self.SingleIonVAET_rotate_in_y:
             self.basis_rotation.run(self)
-        self.vaet.run(self)
-        if self.SingleIonVAET_rotate_out_y:
-            self.basis_rotation.phase_729 = 180.
+        # self.vaet.run(self)
+        # if self.SingleIonVAET_rotate_out_y:
+        #     self.basis_rotation.phase_729 = 180.
+        #     self.basis_rotation.run(self)
 
     
