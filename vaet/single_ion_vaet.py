968fb224a2ff5419b8df91e90c925c8ad221779a
berkeleyions <haeffner-lab@berkeley.edu>
2020-03-13 15:12:42 -0700

    add rotations back in

--- vaet/single_ion_vaet.py
+++ vaet/single_ion_vaet.py
@@ -60,11 +60,11 @@ class SingleIonVAET(PulseSequence):
         self.vaet.phase_ref_time = self.basis_rotation.phase_ref_time
 
         self.stateprep.run(self)
-        # if self.SingleIonVAET_rotate_in_y:
-        #     self.basis_rotation.run(self)
+        if self.SingleIonVAET_rotate_in_y:
+            self.basis_rotation.run(self)
         self.vaet.run(self)
-        # if self.SingleIonVAET_rotate_out_y:
-        #     self.basis_rotation.phase_729 = 180.
-        #     self.basis_rotation.run(self)
+        if self.SingleIonVAET_rotate_out_y:
+            self.basis_rotation.phase_729 = 180.
+            self.basis_rotation.run(self)
 
     
