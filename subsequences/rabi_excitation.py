c28278bb089a580cad6e65777c1a50e67f12ebdc
berkeleyions <haeffner-lab@berkeley.edu>
2020-03-16 17:54:40 -0700

    turn ramp back on rabi

--- subsequences/rabi_excitation.py
+++ subsequences/rabi_excitation.py
@@ -68,4 +68,4 @@ class RabiExcitation:
             delay(r.duration)
             with parallel:
                 self.dds_729.sw.off()
-                #self.dds_729_SP.sw.off()
+                self.dds_729_SP.sw.off()
