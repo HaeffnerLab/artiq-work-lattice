cbd909749cbd6e17de0001d03974002f4b5998ee
Ryan Shaffer <rmshaffer@gmail.com>
2020-05-04 09:35:55 -0400

    auto_calibration

--- auto_calibration/auto_calibration.py
+++ auto_calibration/auto_calibration.py
@@ -23,11 +23,24 @@ class AutoCalibration(PulseSequence):
 
     PulseSequence.scan_params = dict(
         AutoCalibration=[
-            ("Rabi", ("RabiFlopping.duration", 0., 100e-6, 20, "us")),
+            #("Rabi", ("RabiFlopping.duration", 0., 100e-6, 20, "us")),
         ])
 
     def run_initially(self):
         self.set_subsequence["AutoCalibration"] = self.set_subsequence_autocalibration
+
+        calibpitime_expid = {
+            "arguments": {
+                    "CalibPiTime-Scan_Selection": "RabiFlopping.duration",
+                    "CalibPiTime:RabiFlopping.duration": { "ty": "RangeScan", "start": 0.0, "stop": 10.0e-6, "npoints": 20 }
+                },
+            "class_name": "CalibPiTime",
+            "file": "calibration_scans/calib_pi_time.py",
+            "priority": 100,
+            "log_level": 30,
+            "repo_rev": None
+        }
+        self.scheduler.submit("main", calibpitime_expid, priority=100)
 
     @kernel
     def set_subsequence_autocalibration(self):
