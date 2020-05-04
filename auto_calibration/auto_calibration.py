4b5fc73bc8f631b3dd5357bcd15bcb776c0bcdff
Ryan Shaffer <rmshaffer@gmail.com>
2020-05-04 09:47:10 -0400

    auto_calibration

--- auto_calibration/auto_calibration.py
+++ auto_calibration/auto_calibration.py
@@ -14,7 +14,8 @@ class AutoCalibration(PulseSequence):
         "CalibrationScans.calibrate_sidebands",
         "CalibrationScans.do_rabi_flop_carrier",
         "CalibrationScans.do_rabi_flop_radial1",
-        "CalibrationScans.do_rabi_flop_radial2"
+        "CalibrationScans.do_rabi_flop_radial2",
+        "IonsOnCamera.ion_number"
     }
 
     PulseSequence.scan_params = dict(
@@ -25,18 +26,31 @@ class AutoCalibration(PulseSequence):
     def run_initially(self):
         self.set_subsequence["AutoCalibration"] = self.set_subsequence_autocalibration
 
-        calibpitime_expid = {
-            "arguments": {
-                    "CalibPiTime-Scan_Selection": "RabiFlopping.duration",
-                    "CalibPiTime:RabiFlopping.duration": { "ty": "RangeScan", "start": 0.0, "stop": 10.0e-6, "npoints": 20 }
-                },
-            "class_name": "CalibPiTime",
-            "file": "calibration_scans/calib_pi_time.py",
-            "priority": 100,
-            "log_level": 30,
-            "repo_rev": None
-        }
-        self.scheduler.submit("main", calibpitime_expid, priority=100)
+        if self.p.CalibrationScans.do_rabi_flop_carrier:
+            # Run CalibPiTime for carrier
+            calibpitime_expid = {
+                "arguments": {
+                        "CalibPiTime-Scan_Selection": "RabiFlopping.duration",
+                        "CalibPiTime:RabiFlopping.duration": { "ty": "RangeScan", "start": 0.0, "stop": 10.0e-6, "npoints": 20 }
+                    },
+                "class_name": "CalibPiTime",
+                "file": "calibration_scans/calib_pi_time.py",
+                "priority": 100,
+                "log_level": 30,
+                "repo_rev": None
+            }
+            self.scheduler.submit("main", calibpitime_expid, priority=100)
+        
+        if self.p.CalibrationScans.calibrate_sidebands:
+            # TODO - run CalibPiTime for axial sideband
+
+            if self.p.CalibrationScans.do_rabi_flop_radial1:
+                # TODO - run CalibPiTime for radial sideband 1
+                pass
+
+            if self.p.CalibrationScans.do_rabi_flop_radial2:
+                # TODO - run CalibPiTime for radial sideband 1
+                pass
 
     @kernel
     def set_subsequence_autocalibration(self):
