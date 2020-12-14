f7b734de2f1922552dbdc4e257bbf8a5bb803cee
berkeleyions <haeffner-lab@berkeley.edu>
2020-12-13 18:41:52 -0800

    ph

--- subsequences/setup_single_ion_vaet.py
+++ subsequences/setup_single_ion_vaet.py
@@ -86,7 +86,7 @@ class SetupSingleIonVAET:
             freq_red,
             amplitude=s.RSB_amp,
             ref_time_mu=s.phase_ref_time,
-            phase=0.25
+            phase=0.5
         )
 
         self.dds_729.set_att(s.DP_att)
