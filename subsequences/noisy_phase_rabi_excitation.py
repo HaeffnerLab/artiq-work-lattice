909a9a67b43dbf0bc0273a0d0190bdf047036485
berkeleyions <haeffner-lab@berkeley.edu>
2021-01-02 15:29:20 -0800

    adf

--- subsequences/noisy_phase_rabi_excitation.py
+++ subsequences/noisy_phase_rabi_excitation.py
@@ -33,6 +33,4 @@ class RabiExcitation:
         for i in range(0, 100):
             self.dds_729_SP.set_phase(10*i / 360)
             delay(r.duration)
-        with parallel:
-            # self.dds_729.sw.off()
-            self.dds_729_SP.sw.off()
+        self.dds_729_SP.sw.off()
