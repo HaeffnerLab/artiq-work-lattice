2c90ca7e8c83642d3e2bf6de7aad27accd06e9fe
berkeleyions <haeffner-lab@berkeley.edu>
2021-01-01 18:57:44 -0800

    asdf

--- subsequences/noisy_phase_rabi_excitation.py
+++ subsequences/noisy_phase_rabi_excitation.py
@@ -19,30 +19,14 @@ class RabiExcitation:
     def subsequence(self):
         r = RabiExcitation
         self.get_729_dds(r.channel_729)
-
-        if r.ramp_has_been_programmed:
-            self.dds_729.set(r.freq_729,
-                            amplitude=0.,
-                            ref_time_mu=r.phase_ref_time)
-        else:
-            self.dds_729.set(r.freq_729,
-                            amplitude=r.amp_729,
-                            ref_time_mu=r.phase_ref_time)
-            self.dds_729.set_att(r.att_729)
-
+        self.dds_729.set(r.freq_729,
+                        amplitude=r.amp_729,
+                        ref_time_mu=r.phase_ref_time)
+        self.dds_729.set_att(r.att_729)
         sp_freq_729 = 80*MHz + self.get_offset_frequency(r.channel_729)+ 0*3.21022*MHz
         self.dds_729_SP.set(sp_freq_729, amplitude=r.sp_amp_729, 
                          phase=r.phase_729 / 360., ref_time_mu=r.phase_ref_time)
-        self.dds_729_SP.set_att(r.sp_att_729)
-        # print("dp: ", r.frereq_729)
-        # print("sp: ", sp_fq_729)
-        if r.ramp_has_been_programmed:
-            self.dds_729_SP.sw.on()
-            self.execute_pulse_with_amplitude_ramp(
-                dds1_att=r.att_729,
-                dds1_freq=r.freq_729)
-            
-            #self.dds_729_SP.sw.off()
+        self.dds_729_SP.set_att(r.sp_att_729)            
         else:
             with parallel:
                 self.dds_729.sw.on()
