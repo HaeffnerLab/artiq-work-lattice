900849dff9bda9b41d83041fe8ff64f96d42d625
berkeleyions <haeffner-lab@berkeley.edu>
2021-01-01 18:13:53 -0800

    ad

--- qrm/test_frequency_noise.py
+++ qrm/test_frequency_noise.py
@@ -46,6 +46,7 @@ class RabiFlopping(PulseSequence):
     @kernel
     def RabiFlopping(self):
         self.rabi.phase_ref_time = now_mu()
-        for i in range(0, 100):
-            self.rabi.phase_729 = 10.*i
-            self.rabi.run(self)
+        self.rabi.phase_729 = 10.
+        self.rabi.run(self)
+       
+
