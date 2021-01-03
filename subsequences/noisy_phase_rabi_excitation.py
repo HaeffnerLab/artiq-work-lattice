5c7459f13cff1a554d3338bbdab2e5ec640c7346
berkeleyions <haeffner-lab@berkeley.edu>
2021-01-02 17:57:05 -0800

    dfs

--- subsequences/noisy_phase_rabi_excitation.py
+++ subsequences/noisy_phase_rabi_excitation.py
@@ -26,7 +26,7 @@ class RabiExcitation:
         sp_freq_729 = 10*MHz + self.get_offset_frequency(r.channel_729)
         self.dds_729_SP.set(sp_freq_729, amplitude=r.sp_amp_729, 
                          phase=r.phase_729 / 360.)#, ref_time_mu=r.phase_ref_time)
-        self.dds_729_SP_bichro.set(sp_freq_729, amplitude=r.sp_amp_729, 
+        self.dds_729_SP_bichro.set(1*MHz, amplitude=r.sp_amp_729, 
                          phase=r.phase_729 / 360.)#, ref_time_mu=r.phase_ref_time)         
         self.dds_729_SP.set_att(r.sp_att_729)
         self.dds_729_SP_bichro.set_att(r.sp_att_729)           
