b00c5fe6c8992124fcc72fb180c1cf3092c43739
berkeleyions <haeffner-lab@berkeley.edu>
2021-01-02 17:06:22 -0800

    adsf

--- subsequences/noisy_phase_rabi_excitation.py
+++ subsequences/noisy_phase_rabi_excitation.py
@@ -34,12 +34,13 @@ class RabiExcitation:
             self.dds_729.sw.on()
             self.dds_729_SP.sw.on()
             self.dds_729_SP_bichro.sw.on()
-        for i in range(0, 10):
-            start_mu = now_mu()
-            self.dds_729_SP.set(sp_freq_729, phase=(i%2) * 0.5, ref_time_mu=start_mu)
-            at_mu(start_mu)
-            self.dds_729_SP_bichro.set(sp_freq_729, phase=-(i%2) * 0.5, ref_time_mu=start_mu)
-            delay(r.duration)
+        with parallel:
+            for i in range(0, 10):
+                self.dds_729_SP.set(sp_freq_729, phase=(i%2) * 0.5)
+                delay(r.duration)
+            for i in range(0, 10):
+                self.dds_729_SP_bichro.set(sp_freq_729, phase=-(i%2) * 0.5)
+                delay(r.duration)
         with parallel:
             self.dds_729_SP.sw.off()
             self.dds_729_SP_bichro.sw.off()
