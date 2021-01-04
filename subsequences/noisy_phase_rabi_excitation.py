cbcb726f10000bc5d9aa456cad7048039448f9c4
berkeleyions <haeffner-lab@berkeley.edu>
2021-01-03 19:42:53 -0800

    adsf

--- subsequences/noisy_phase_rabi_excitation.py
+++ subsequences/noisy_phase_rabi_excitation.py
@@ -25,9 +25,9 @@ class NoisyPhaseRabiExcitation:
         self.dds_729.set_att(r.att_729)
         sp_freq_729 = 80*MHz + self.get_offset_frequency(r.channel_729)
         self.dds_729_SP.set(sp_freq_729, amplitude=r.sp_amp_729, 
-                         phase=r.phase_729 / 360.)#, ref_time_mu=r.phase_ref_time)
+                         phase=r.phase_729 / 360., ref_time_mu=r.phase_ref_time)
         self.dds_729_SP_bichro.set(1*MHz, amplitude=r.sp_amp_729, 
-                         phase=r.phase_729 / 360.)#, ref_time_mu=r.phase_ref_time)         
+                         phase=r.phase_729 / 360., ref_time_mu=r.phase_ref_time)         
         self.dds_729_SP.set_att(r.sp_att_729)
         self.dds_729_SP_bichro.set_att(r.sp_att_729)           
         with parallel:
@@ -39,6 +39,6 @@ class NoisyPhaseRabiExcitation:
                 self.dds_729_SP_bichro.set(1*MHz, phase=r.noise_list[i])
             with sequential:
                 delay(r.duration)
-            with parallel:
-                self.dds_729_SP.sw.off()
-                self.dds_729_SP_bichro.sw.off()
+                with parallel:
+                    self.dds_729_SP.sw.off()
+                    self.dds_729_SP_bichro.sw.off()
