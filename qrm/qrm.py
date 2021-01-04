b8ad3b92c548a02a04e31651abc364810c8c12b8
berkeleyions <haeffner-lab@berkeley.edu>
2021-01-03 19:31:55 -0800

    asdf

--- qrm/qrm.py
+++ qrm/qrm.py
@@ -30,7 +30,7 @@ class QRM(PulseSequence):
         self.set_subsequence["QRM"] = self.set_subsequence_qrm
 
     def get_random_list(self) -> TList(TFloat):
-        return list(np.random.randn(100)) 
+        return list(np.random.randn(1000)) 
 
     @kernel
     def set_subsequence_qrm(self):
