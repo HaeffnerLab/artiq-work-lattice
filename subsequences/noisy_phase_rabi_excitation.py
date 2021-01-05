45e967b98b1aef617667d36dda065267fdccf319
berkeleyions <haeffner-lab@berkeley.edu>
2021-01-04 19:40:02 -0800

    asdf

--- subsequences/noisy_phase_rabi_excitation.py
+++ subsequences/noisy_phase_rabi_excitation.py
@@ -42,7 +42,7 @@ class NoisyPhaseRabiExcitation:
         #     ramp_duration=2.0*us,
         #     dds1_amp=b.amp)
         # b.ramp_has_been_programmed = True
-        self.core.break_realtime()
+        pulse_sequence.core.break_realtime()
         delay(12*ms)
         pulse_sequence.prepare_noisy_single_pass(freq_noise=True)   
     
