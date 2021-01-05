096dae2ccb8021b00241063bbf46e48cfedef2a2
berkeleyions <haeffner-lab@berkeley.edu>
2021-01-04 18:16:35 -0800

    adf

--- subsequences/noisy_phase_rabi_excitation.py
+++ subsequences/noisy_phase_rabi_excitation.py
@@ -24,7 +24,7 @@ class NoisyPhaseRabiExcitation:
         r = NoisyPhaseRabiExcitation
         pulse_sequence.generate_single_pass_noise_waveform(
             mean=0,
-            std=r.noise_parameter,
+            std=93.,
             freq_noise=True)
     
     def subsequence(self):
