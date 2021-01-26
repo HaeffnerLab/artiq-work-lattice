3c7284cae47ecf7d7ed60352275d04d5e13d0d7e
berkeleyions <haeffner-lab@berkeley.edu>
2021-01-25 21:04:25 -0800

    ph

--- subsequences/setup_single_ion_vaet.py
+++ subsequences/setup_single_ion_vaet.py
@@ -57,7 +57,7 @@ class SetupSingleIonVAET:
                 freq_carr,
                 amplitude=s.delta_amp,
                 ref_time_mu=s.phase_ref_time,
-                phase=0.551  # sigma_y
+                phase=0.251  # sigma_y
             )
         else:
             self.dds_729_SP.set(
