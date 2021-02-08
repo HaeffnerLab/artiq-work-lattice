df4d33f528098fc3d948b6530b1e2618ca94fe71
berkeleyions <haeffner-lab@berkeley.edu>
2021-02-08 13:00:23 -0800

    uncomment channel

--- subsequences/setup_single_ion_vaet.py
+++ subsequences/setup_single_ion_vaet.py
@@ -136,8 +136,8 @@ class SetupSingleIonVAET:
         self.dds_729.set_att(s.DP_att)
         self.dds_729_SP.set_att(s.J_att)
         self.dds_729_SP_bichro.set_att(s.delta_att)
-        #self.dds_SP_729L1_bichro.set_att(s.noise_att)
-        self.dds_729L1.set_att(s.noise_att)
+        self.dds_SP_729L1_bichro.set_att(s.noise_att)
+        #self.dds_729L1.set_att(s.noise_att)
         self.dds_729_SP_line1_bichro.set_att(s.BSB_att)
         self.dds_729_SP_line2_bichro.set_att(s.RSB_att)
 
