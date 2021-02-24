130fe4ea157ae5922bc613077d0070861ac58445
berkeleyions <haeffner-lab@berkeley.edu>
2021-02-24 10:17:46 -0800

    ph test

--- subsequences/setup_single_ion_vaet.py
+++ subsequences/setup_single_ion_vaet.py
@@ -91,7 +91,7 @@ class SetupSingleIonVAET:
                     freq_carr,
                     amplitude=s.noise_amp,
                     ref_time_mu=s.phase_ref_time,
-                    phase=0.38
+                    phase=0.38+0.494
 
                 )
                 # self.dds_729L1.set(
