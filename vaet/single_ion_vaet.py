2d9d4dac5511239d44abafaee04e165a98c0036e
berkeleyions <haeffner-lab@berkeley.edu>
2021-03-20 23:41:57 -0700

    ph

--- vaet/single_ion_vaet.py
+++ vaet/single_ion_vaet.py
@@ -81,7 +81,7 @@ class SingleIonVAET(PulseSequence):
         self.vaet.phase_ref_time = self.basis_rotation.phase_ref_time
 
         #setting up a ref rf for testing phase
-        self.dds_SP_729L1.set(80.3*MHz, amplitude=0.1, phase=0.4, ref_time_mu=self.basis_rotation.phase_ref_time)
+        self.dds_SP_729L1.set(80.3*MHz, amplitude=0.5, phase=0.4, ref_time_mu=self.basis_rotation.phase_ref_time)
         self.dds_SP_729L1.set_att(5*dB)
 
         self.stateprep.run(self)
