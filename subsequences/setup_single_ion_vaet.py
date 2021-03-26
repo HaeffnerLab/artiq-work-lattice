98a49f4baf43765ef6f70bc1d901807eb657df8c
berkeleyions <haeffner-lab@berkeley.edu>
2021-03-26 16:36:24 -0700

    ph

--- subsequences/setup_single_ion_vaet.py
+++ subsequences/setup_single_ion_vaet.py
@@ -60,14 +60,14 @@ class SetupSingleIonVAET:
                     freq_carr,
                     amplitude=s.delta_amp,
                     ref_time_mu=s.phase_ref_time,
-                    phase=0.4  # sigma_y 0.381
+                    phase=0.39  # sigma_y 0.381
                 )
                 #hard code noise to L1_SP_Bichro or 729 L1
                 self.dds_SP_729L1_bichro.set(
                     freq_carr,
                     amplitude=s.noise_amp,
                     ref_time_mu=s.phase_ref_time,
-                    phase=0.4   #0.381
+                    phase=0.39   #0.381
 
                 )
 
