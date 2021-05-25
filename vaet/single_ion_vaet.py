aedc2e635642a17c090c37da3762889f3a36b856
berkeleyions <haeffner-lab@berkeley.edu>
2021-05-24 17:23:57 -0700

    test

--- vaet/single_ion_vaet.py
+++ vaet/single_ion_vaet.py
@@ -82,7 +82,7 @@ class SingleIonVAET(PulseSequence):
 
     @kernel
     def SingleIonVAET(self):
-        self.basis_rotation.phase_ref_time = now_mu()
+        # self.basis_rotation.phase_ref_time = now_mu()
         self.vaet.phase_ref_time = self.basis_rotation.phase_ref_time
         # print(self.basis_rotation.phase_ref_time)
         # print(self.basis_rotation.phase_ref_time)
