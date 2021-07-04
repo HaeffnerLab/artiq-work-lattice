0e8c7ef2feaf6ce606f828c002186f67def8a2b1
berkeleyions <haeffner-lab@berkeley.edu>
2021-07-04 12:40:57 -0700

    asdf

--- subsequences/setup_single_ion_vaet.py
+++ subsequences/setup_single_ion_vaet.py
@@ -136,8 +136,7 @@ class SetupSingleIonVAET:
         self.dds_729_SP_bichro.set_att(s.delta_att)
         self.dds_SP_729L1.set_att(s.noise_att)
 
-        if s.with_noise:
-            pass
+        # if s.with_noise:
         #     j = round(self.get_variable_parameter("current_experiment_iteration"))
         #     if s.noise_type == "white_delta" or s.noise_type == "lorentzian_delta":
         #         self.setup_ram_modulation(
@@ -162,7 +161,6 @@ class SetupSingleIonVAET:
             #                         step=s.step,
             #                         ram_mode=RAM_MODE_RAMPUP
             #                     )
-            delay(2*ms)   
 
         self.dds_729.sw.on()
         with parallel:
