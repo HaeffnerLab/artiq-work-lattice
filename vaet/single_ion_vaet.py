96229e74b54daff417c367a4d89e9e7ebb0ab4a5
berkeleyions <haeffner-lab@berkeley.edu>
2021-07-09 20:27:23 -0700

    asdf

--- vaet/single_ion_vaet.py
+++ vaet/single_ion_vaet.py
@@ -137,7 +137,7 @@ class SingleIonVAET(PulseSequence):
         rng = np.random.default_rng()
         
         for i in range(m):
-            n = 100
+            n = 1000
             amp_wf = [j%2 * 1. for j in range(n)]
             phase_wf = [0. for j in range(n)]
             ram_wf = [0] * n
