e20a496308abe0cce36edfca2595dd0658273c4e
berkeleyions <haeffner-lab@berkeley.edu>
2021-07-09 19:48:26 -0700

    adf

--- vaet/single_ion_vaet.py
+++ vaet/single_ion_vaet.py
@@ -138,7 +138,7 @@ class SingleIonVAET(PulseSequence):
         
         for i in range(m):
             amp_wf = [j%2 * 1. for j in range(100)]
-            phase_wf = [0.]
+            phase_wf = [0. for j in range(100)]
             ram_wf = [0] * 100
             self.turns_amplitude_to_ram(phase_wf, amp_wf, ram_wf)
             self.vaet.mod_wf.append(np.int32(ram_wf))
