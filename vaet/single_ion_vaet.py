29f58ecb49d3fa5c3b342c4b03ad054296334866
berkeleyions <haeffner-lab@berkeley.edu>
2021-07-12 18:25:32 -0700

    adsf

--- vaet/single_ion_vaet.py
+++ vaet/single_ion_vaet.py
@@ -128,7 +128,8 @@ class SingleIonVAET(PulseSequence):
 
     def setup_noise_waveforms(self, n, m):
         noise_time_step = 2*us  # 1/sampling rate
-        self.vaet.step = 400#round(noise_time_step / 4*ns)
+        self.vaet.step = round(noise_time_step / 4*ns)
+        print(self.vaet.step)
         noise_type = self.p.SingleIonVAET.noise_type
         if "delta" in noise_type:
             self.vaet.amplitude_noise = True
