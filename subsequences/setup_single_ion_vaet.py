78cfd045bf42073f7d64efd5a6f6a0a1ce39d2f7
berkeleyions <haeffner-lab@berkeley.edu>
2021-08-01 14:16:07 -0700

    asdf

--- subsequences/setup_single_ion_vaet.py
+++ subsequences/setup_single_ion_vaet.py
@@ -89,19 +89,19 @@ class SetupSingleIonVAET:
         self.dds_729_SP.set_att(s.CARR_att)
         self.dds_729_SP_bichro.set_att(s.BSB_att)
         self.dds_SP_729L2.set_att(s.RSB_att)
-        self.dds_test1.set(freq_carr, amplitude=1.0, ref_time_mu=s.phase_ref_time)
+        self.dds_test1.set(freq_carr, amplitude=1.0, phase_mode=phase_mode, ref_time_mu=s.phase_ref_time)
         self.dds_test1.set_att(5.)  # for beat note
 
         self.dds_729.sw.on()
         with parallel:
             self.trigger.on()
+            self.dds_test1.sw.on()  # for beat note
             self.dds_729_SP.cpld.io_update.pulse_mu(8)
             self.dds_729_SP_bichro.cpld.io_update.pulse_mu(8)
             self.dds_SP_729L2.cpld.io_update.pulse_mu(8)
             self.dds_729_SP.sw.on()
             self.dds_729_SP_bichro.sw.on()
             self.dds_SP_729L2.sw.on()
-            self.dds_test1.sw.on()  # for beat note
         
         delay(s.duration)
         
