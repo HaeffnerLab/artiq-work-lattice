335d778c58c56d3f38fc9847c6fcc0dc87217933
berkeleyions <haeffner-lab@berkeley.edu>
2021-08-03 18:17:48 -0700

    adf

--- subsequences/rabi_excitation.py
+++ subsequences/rabi_excitation.py
@@ -42,11 +42,11 @@ class RabiExcitation:
             self.dds_729.set(r.freq_729,
                             amplitude=0.,
                             ref_time_mu=r.phase_ref_time)
-        # else:
-        self.dds_729.set(r.freq_729,
-                        amplitude=r.amp_729,
-                        ref_time_mu=r.phase_ref_time)
-        self.dds_729.set_att(r.att_729)
+        else:
+            self.dds_729.set(r.freq_729,
+                            amplitude=r.amp_729,
+                            ref_time_mu=r.phase_ref_time)
+            self.dds_729.set_att(r.att_729)
 
         sp_freq_729 = 80*MHz + self.get_offset_frequency(r.channel_729)+ 0*3.21022*MHz
         self.dds_729_SP.set(sp_freq_729, amplitude=r.sp_amp_729, 
@@ -64,7 +64,7 @@ class RabiExcitation:
         else:
             with parallel:
                 self.dds_729.sw.on()
-                # self.dds_729_SP.sw.on()
+                self.dds_729_SP.sw.on()
             delay(r.duration)
             with parallel:
                 self.dds_729.sw.off()
