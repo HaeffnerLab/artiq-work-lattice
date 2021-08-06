eb00f8fbeab6ce7db62dcabc5a8c4fbb0db37462
berkeleyions <haeffner-lab@berkeley.edu>
2021-08-05 22:22:10 -0700

    adf

--- calibration_scans/optimize_sideband_cooling.py
+++ calibration_scans/optimize_sideband_cooling.py
@@ -1,6 +1,7 @@
 from pulse_sequence import PulseSequence
 from subsequences.rabi_excitation import RabiExcitation
 from subsequences.state_preparation import StatePreparation
+from subsequences.sideband_cooling import SidebandCooling
 from artiq.experiment import *
 
 class OptimizeSidebandCooling(PulseSequence):
@@ -33,6 +34,7 @@ class OptimizeSidebandCooling(PulseSequence):
 
     def run_initially(self):
         self.stateprep = self.add_subsequence(StatePreparation)
+        self.sidebandcooling = self.add_subsequence(SidebandCooling)
         self.rabi = self.add_subsequence(RabiExcitation)
         self.rabi.channel_729 = self.p.RabiFlopping.channel_729
         self.set_subsequence["krun"] = self.set_subsequence_krun
@@ -43,18 +45,20 @@ class OptimizeSidebandCooling(PulseSequence):
         self.rabi.amp_729 = self.RabiFlopping_amplitude_729
         self.rabi.att_729 = self.RabiFlopping_att_729
         self.rabi.freq_729 = self.calc_frequency(
-            self.RabiFlopping_line_selection, 
-            detuning=0.,
-            sideband=self.RabiFlopping_selection_sideband,
-            order=self.RabiFlopping_order, 
-            dds=self.RabiFlopping_channel_729
-        )
-        self.stateprep.sbc.att_729 = self.get_variable_parameter("SidebandCooling_att_729")
-        self.stateprep.sbc.att_854 = self.get_variable_parameter("SidebandCooling_att_854")
-        self.stateprep.sbc.stark_shift = self.get_variable_parameter("SidebandCooling_stark_shift")
+                                            self.RabiFlopping_line_selection, 
+                                            detuning=0.,
+                                            sideband=self.RabiFlopping_selection_sideband,
+                                            order=self.RabiFlopping_order, 
+                                            dds=self.RabiFlopping_channel_729
+                                        )
+        self.stateprep.enable_sideband_cooling = False
+        self.sidebandcooling.att_729 = self.get_variable_parameter("SidebandCooling_att_729")
+        self.sidebandcooling.att_854 = self.get_variable_parameter("SidebandCooling_att_854")
+        self.sidebandcooling.stark_shift = self.get_variable_parameter("SidebandCooling_stark_shift")
     
     @kernel
     def krun(self):
         self.stateprep.run(self)
+        self.sidebandcooling.run(self)
         self.rabi.run(self)
 #
