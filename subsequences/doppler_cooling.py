92a8be38f04a52a60919cc28c97ef8e9d641a131
berkeleyions <haeffner-lab@berkeley.edu>
2021-08-08 12:58:06 -0700

    adsf

--- subsequences/doppler_cooling.py
+++ subsequences/doppler_cooling.py
@@ -11,20 +11,21 @@ class DopplerCooling:
     frequency_866="DopplerCooling.doppler_cooling_frequency_866"
     amplitude_866="DopplerCooling.doppler_cooling_amplitude_866"
     att_866="DopplerCooling.doppler_cooling_att_866"
+    # recrystallize_pulse="DopplerCooling.recrystallize_pulse"
 
     def subsequence(self):
         d = DopplerCooling
-        self.dds_397.set(60*MHz, amplitude=d.amplitude_397)
+        self.dds_397.set(d.frequency_397, amplitude=d.amplitude_397)
         self.dds_397.set_att(d.att_397)
         self.dds_866.set(d.frequency_866, amplitude=d.amplitude_866)
         self.dds_866.set_att(d.att_866)
         with parallel:
             self.dds_397.sw.on()
             self.dds_866.sw.on()
-        delay(d.pre_duration)
-        self.dds_397.set(d.frequency_397, amplitude=d.amplitude_397)
+        # if d.recrystallize_pulse:
+        #     self.dds_397.set(60*MHz, amplitude=1.0)
+        #     delay(d.pre_duration)
         delay(d.duration)
         self.dds_397.sw.off()
-        self.dds_397.set(20*MHz, amplitude=0.)
-        delay(d.additional_repump_duration)
+        delay(10*us)
         self.dds_866.sw.off()
