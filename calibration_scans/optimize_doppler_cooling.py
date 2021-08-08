ce2545a3544169f5e709232edee44d04719ebfc3
berkeleyions <haeffner-lab@berkeley.edu>
2021-08-08 15:06:28 -0700

    adsf

--- calibration_scans/optimize_doppler_cooling.py
+++ calibration_scans/optimize_doppler_cooling.py
@@ -21,7 +21,8 @@ class OptimizeDopplerCooling(PulseSequence):
     }
 
     PulseSequence.scan_params["optimize_doppler_cooling"] = [
-        ("Current", ("DopplerCooling.doppler_cooling_frequency_397", 50*MHz, 70*MHz, 20, "MHz")),
+        ("Current", ("OpticalPumping.optical_pumping_frequency_854", -2*MHz, 2*MHz, 20, "MHz")),
+        # ("Current", ("DopplerCooling.doppler_cooling_frequency_397", 50*MHz, 70*MHz, 20, "MHz")),
         # ("Current", ("DopplerCooling.doppler_cooling_amplitude_397", 0., 1., 20)),
         # ("Current", ("DopplerCooling.doppler_cooling_duration", 0, 4*ms, 20, "ms")),
     ]
