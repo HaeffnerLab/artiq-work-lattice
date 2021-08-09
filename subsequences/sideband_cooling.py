79b6e4f8cba31bdf7b53b2d875bc19b9e4307911
berkeleyions <haeffner-lab@berkeley.edu>
2021-08-09 13:31:07 -0700

    adf

--- subsequences/sideband_cooling.py
+++ subsequences/sideband_cooling.py
@@ -118,32 +118,32 @@ class SidebandCooling:
             s.sp_freq_729_sequential2 = 80*MHz + self.get_offset_frequency(s.sequential2_channel_729)
 
         for i in range(num_cycles):
-            s.run_sideband_cooling(s, s.channel_729, s.freq_729, s.sp_freq_729)
-            s.fast_op(s)
+            s.run_sideband_cooling(self, s, s.channel_729, s.freq_729, s.sp_freq_729)
+            s.fast_op(self, s)
             
             if s.sequential_enable:
-                s.run_sideband_cooling(s,
+                s.run_sideband_cooling(self, s,
                                 s.sequential_channel_729, 
                                 s.freq_729_sequential,
                                 s.sp_freq_729_sequential
                             )
-                s.fast_op(s)
+                s.fast_op(self, s)
 
             if s.sequential1_enable:
-                s.run_sideband_cooling(s,
+                s.run_sideband_cooling(self, s,
                                 s.sequential1_channel_729,
                                 s.freq_729_sequential1,
                                 s.sp_freq_729_sequential1
                             )
-                s.fast_op(s)
+                s.fast_op(self, s)
 
             if s.sequential2_enable:
-                s.run_sideband_cooling(s,
+                s.run_sideband_cooling(self, s,
                                 s.sequential2_channel_729,
                                 s.freq_729_sequential2,
                                 s.sp_freq_729_sequential2
                             )
-                s.fast_op(s)
+                s.fast_op(self, s)
             
         self.dds_854.set(80*MHz, amplitude=1.0)
         self.dds_854.set_att(5.0)
@@ -157,7 +157,7 @@ class SidebandCooling:
             self.dds_854.sw.off()
             self.dds_866.sw.off()
 
-    def run_sideband_cooling(self, channel, freq_729, sp_freq_729):
+    def run_sideband_cooling(self, s, channel, freq_729, sp_freq_729):
         self.get_729_dds(channel)
         self.dds_729.set(freq_729, amplitude=s.amplitude_729)
         self.dds_729.set_att(s.att_729)
@@ -180,7 +180,7 @@ class SidebandCooling:
             self.dds_729.sw.off()
             #self.dds_729_SP.sw.off()  keep SP on all the time 2/24/2020
         
-    def fast_op(self):
+    def fast_op(self, s):
         self.get_729_dds(s.op_channel_729)
         self.dds_729.set(
                         s.op_freq_729, 
