2ebdfbf86817dd570aabb23b5642e859c90129ab
berkeleyions <haeffner-lab@berkeley.edu>
2021-08-09 11:00:05 -0700

    adfs

--- subsequences/sideband_cooling.py
+++ subsequences/sideband_cooling.py
@@ -71,7 +71,7 @@ class SidebandCooling:
                         )
         sp_freq_729 = 80*MHz + self.get_offset_frequency(s.channel_729)
         
-        op_freq_729 = self.calc_frequency(
+        s.op_freq_729 = self.calc_frequency(
                                 s.op_line_selection,
                                 dds=s.op_channel_729
                             )
@@ -114,7 +114,7 @@ class SidebandCooling:
         def fast_op():
             self.get_729_dds(s.op_channel_729)
             self.dds_729.set(
-                        op_freq_729, 
+                        s.op_freq_729, 
                          amplitude=s.op_amplitude_729
                     )
             self.dds_729.set_att(s.op_att_729)
