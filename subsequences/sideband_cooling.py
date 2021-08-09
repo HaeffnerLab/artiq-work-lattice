e38f31c91b75b8b0020d3303edade4776562f861
berkeleyions <haeffner-lab@berkeley.edu>
2021-08-08 23:40:35 -0700

    adf

--- subsequences/sideband_cooling.py
+++ subsequences/sideband_cooling.py
@@ -78,7 +78,7 @@ class SidebandCooling:
                 self.dds_866.sw.off()
                 self.dds_729.sw.off()
                 #self.dds_729_SP.sw.off()  keep SP on all the time 2/24/2020
-            s.op.run(self)
+            # s.op.run(self)
 
         i = 0
         for i in range(num_cycles):
